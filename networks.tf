ebbe8e3675470c40247b1e28335c62fd063b8cca
Pratik Mallya <pratik.mallya@liveramp.com>
2018-09-26 13:16:45 -0700

    Better way to access networks
    Use data resource rather than path formatting

---
+++ networks.tf
@@ -0,0 +1,9 @@
+data "google_compute_network" "gke_network" {
+  name    = "${var.network}"
+  project = "${local.network_project_id}"
+}
+
+data "google_compute_subnetwork" "gke_subnetwork" {
+  name    = "${var.subnetwork}"
+  project = "${local.network_project_id}"
+}
