219faca6f0ea368441427012a8faf009e27e1079
Pratik Mallya <pratik.mallya@liveramp.com>
2018-09-26 21:47:19 -0700

    Fix lint issues

--- test/make.sh
+++ test/make.sh
@@ -88,7 +88,7 @@ function check_trailing_whitespace() {
 function generate_docs() {
   echo "Generating markdown docs with terraform-docs"
   TMPFILE=$(mktemp)
-  for j in `for i in $(find . -type f | grep \.tf$) ; do dirname $i ; done | sort -u` ; do
+  for j in $(for i in $(find . -type f | grep \.tf$) ; do dirname $i ; done | sort -u) ; do
     terraform-docs markdown "$j" > "$TMPFILE"
     python helpers/combine_docfiles.py "$j"/README.md "$TMPFILE"
   done
