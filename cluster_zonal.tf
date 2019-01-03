5f49332b1158fdae099917b88993f6c449338f6d
Mateusz Kubaczyk <kubaczyk@gmail.com>
2019-01-03 15:19:40 +0100

    Add full path for network/subnetwork ommiting  permadiff caused by remove_default_node_pool

--- cluster_zonal.tf
+++ cluster_zonal.tf
@@ -26,8 +26,8 @@ resource "google_container_cluster" "zonal_primary" {
   zone             = "${var.zones[0]}"
   additional_zones = ["${slice(var.zones,1,length(var.zones))}"]
 
-  network            = "${data.google_compute_network.gke_network.self_link}"
-  subnetwork         = "${data.google_compute_subnetwork.gke_subnetwork.self_link}"
+  network            = "projects/${var.project_id}/global/networks/${data.google_compute_network.gke_network.name}"
+  subnetwork         = "projects/${var.project_id}/regions/${var.region}/subnetworks/${data.google_compute_subnetwork.gke_subnetwork.name}"
   min_master_version = "${local.kubernetes_version}"
 
   logging_service    = "${var.logging_service}"
