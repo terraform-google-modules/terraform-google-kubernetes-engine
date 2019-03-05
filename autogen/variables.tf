077e6ec61917b7e372fbce932f5a2c4bd78a8191
Jason Berlinsky <jason@barefootcoders.com>
2019-03-04 23:29:24 -0500

    Change service account default to create a new SA per cluster

--- autogen/variables.tf
+++ autogen/variables.tf
@@ -214,7 +214,7 @@ variable "monitoring_service" {
 
 variable "service_account" {
   description = "The service account to default running nodes as if not overridden in `node_pools`. Defaults to the compute engine default service account. May also specify `create` to automatically create a cluster-specific service account"
-  default     = ""
+  default     = "create"
 }
 {% if private_cluster %}
 
