cbfc3c76bccbbb9fc43e66b56dfcc9e148b31a4e
Aaron Lane <aarondrewlane@gmail.com>
2019-04-04 13:50:36 -0400

    Isolate service_account_resource_name test output

---
+++ test/fixtures/simple_zonal/unique_outputs.tf
@@ -0,0 +1,20 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+output "service_account_resource_name" {
+  description = "The resource name of the service account to default running nodes as if not overridden in `node_pools`."
+  value       = "${module.gke.service_account_resource_name}"
+}
