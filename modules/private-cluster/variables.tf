022561a80129d1c5ad2b1569922019db4d27f5bc
Aaron Lane <aarondrewlane@gmail.com>
2019-04-05 11:04:54 -0400

    Add reference to migration guide and regenerate

--- modules/private-cluster/variables.tf
+++ modules/private-cluster/variables.tf
@@ -129,6 +129,7 @@ variable "disable_legacy_metadata_endpoints" {
   default     = "true"
 }
 
+
 variable "node_pools" {
   type        = "list"
   description = "List of maps containing node pools"
@@ -216,33 +217,32 @@ variable "service_account" {
   description = "The service account to run nodes as if not overridden in `node_pools`. The default value will cause a cluster-specific service account to be created."
   default     = "create"
 }
-
 variable "enable_private_endpoint" {
-  description = "(Beta) Whether the master's internal IP address is used as the cluster endpoint"
-  default     = false
+  description  = "(Beta) Whether the master's internal IP address is used as the cluster endpoint"
+  default      = false
 }
 
 variable "enable_private_nodes" {
-  description = "(Beta) Whether nodes have internal IP addresses only"
-  default     = false
+  description  = "(Beta) Whether nodes have internal IP addresses only"
+  default      = false
 }
 
 variable "master_ipv4_cidr_block" {
-  description = "(Beta) The IP range in CIDR notation to use for the hosted master network"
-  default     = "10.0.0.0/28"
+  description  = "(Beta) The IP range in CIDR notation to use for the hosted master network"
+  default      = "10.0.0.0/28"
 }
 
 variable "basic_auth_username" {
   description = "The username to be used with Basic Authentication. An empty value will disable Basic Authentication, which is the recommended configuration."
-  default     = ""
+  default = ""
 }
 
 variable "basic_auth_password" {
   description = "The password to be used with Basic Authentication."
-  default     = ""
+  default = ""
 }
 
 variable "issue_client_certificate" {
   description = "Issues a client certificate to authenticate to the cluster endpoint. To maximize the security of your cluster, leave this option disabled. Client certificates don't automatically rotate and aren't easily revocable. WARNING: changing this after cluster creation is destructive!"
-  default     = "false"
+  default = "false"
 }
