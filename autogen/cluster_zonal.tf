42e185d4a879e55da9e34c5381831eb10abb929a
Ihor Pukha <sorceress1979@gmail.com>
2019-06-05 18:54:04 +0300

    Add configuration flag for `pod_security_policy_config` #140

--- autogen/cluster_zonal.tf
+++ autogen/cluster_zonal.tf
@@ -44,6 +44,7 @@ resource "google_container_cluster" "zonal_primary" {
 
 {% if private_cluster %}
   enable_binary_authorization = "${var.enable_binary_authorization}"
+  pod_security_policy_config  = "${var.pod_security_policy_config}"
 {% endif %}
   master_authorized_networks_config = ["${var.master_authorized_networks_config}"]
 
