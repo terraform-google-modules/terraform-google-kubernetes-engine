59563ff972a8a7f7c4b58867fe2e33e52cddd9cd
Ingwarr <sorceress1979@gmail.com>
2019-06-06 18:13:57 +0300

    Update autogen/cluster_zonal.tf
    Co-Authored-By: Aaron Lane <aarondrewlane@gmail.com>

--- autogen/cluster_zonal.tf
+++ autogen/cluster_zonal.tf
@@ -43,8 +43,8 @@ resource "google_container_cluster" "zonal_primary" {
   monitoring_service = "${var.monitoring_service}"
 
 {% if private_cluster %}
-  enable_binary_authorization = "${var.enable_binary_authorization}"
-  pod_security_policy_config  = "${var.pod_security_policy_config}"
+  enable_binary_authorization       = "${var.enable_binary_authorization}"
+  pod_security_policy_config        = "${var.pod_security_policy_config}"
 {% endif %}
   master_authorized_networks_config = ["${var.master_authorized_networks_config}"]
 
