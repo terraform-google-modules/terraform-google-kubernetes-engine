61dc085f7a4d5e406149c3bf26c6d1243a5226b9
Aaron Lane <aarondrewlane@gmail.com>
2019-06-18 15:01:13 -0400

    Exclude shared fixture from validation

--- test/make.sh
+++ test/make.sh
@@ -45,11 +45,12 @@ function docker() {
 # files ending in '.tf'
 function check_terraform() {
   echo "Running terraform validate"
-  #shellcheck disable=SC2038
   find . -name "*.tf" \
     -not -path "./autogen/*" \
     -not -path "./test/fixtures/all_examples/*" \
-    | xargs dirname | sort | uniq | xargs -L 1 -i{} bash -c 'terraform init "{}" > /dev/null && terraform validate "{}" && terraform fmt -check=true -write=false "{}"'
+    -not -path "./test/fixtures/shared/*" \
+    -print0 \
+    | xargs -0 dirname | sort | uniq | xargs -L 1 -i{} bash -c 'terraform init "{}" > /dev/null && terraform validate "{}" && terraform fmt -check=true -write=false "{}"'
 }
 
 # This function runs 'go fmt' and 'go vet' on every file
