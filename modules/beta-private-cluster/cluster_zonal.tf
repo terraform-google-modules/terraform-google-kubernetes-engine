2abcacc9ef1ee209c1c41f063da8a96a583bd72c
sylvio.pedroza <sylvio.pedroza@skipthedishes.ca>
2019-08-14 11:15:16 -0600

    add workload identity support to beta-private-cluster

--- modules/beta-private-cluster/cluster_zonal.tf
+++ modules/beta-private-cluster/cluster_zonal.tf
@@ -169,6 +169,10 @@ resource "google_container_cluster" "zonal_primary" {
       state    = database_encryption.value.state
     }
   }
+
+  workload_identity_config {
+    identity_namespace = var.identity_namespace
+  }
 }
 
 /******************************************
