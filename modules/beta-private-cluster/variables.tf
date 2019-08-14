5ebccee2153f3465e1576026a4d07de0828e614a
sylvio.pedroza <sylvio.pedroza@skipthedishes.ca>
2019-08-14 11:15:16 -0600

    add workload identity support to beta-private-cluster

--- modules/beta-private-cluster/variables.tf
+++ modules/beta-private-cluster/variables.tf
@@ -373,3 +373,9 @@ variable "enable_vertical_pod_autoscaling" {
   description = "Vertical Pod Autoscaling automatically adjusts the resources of pods controlled by it"
   default     = false
 }
+
+variable "identity_namespace" {
+  type        = string
+  description = "Workload Identity namespace"
+  default     = ""
+}
