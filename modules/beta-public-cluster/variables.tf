dd81bc0478f696aaf0cf62252800910f39a8f380
sylvio.pedroza <sylvio.pedroza@skipthedishes.ca>
2019-08-16 18:48:30 -0600

    workload identity dynamic

--- modules/beta-public-cluster/variables.tf
+++ modules/beta-public-cluster/variables.tf
@@ -351,8 +351,9 @@ variable "enable_vertical_pod_autoscaling" {
 }
 
 variable "identity_namespace" {
-  type        = string
   description = "Workload Identity namespace"
-  default     = null
+  type        = list(string)
+  default     = []
 }
+
 
