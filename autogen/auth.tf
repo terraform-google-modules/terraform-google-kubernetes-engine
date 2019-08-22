9e953598c42292b86fae9b10c8b0a9149e84018f
anantasty <anant.asty@gmail.com>
2019-08-21 22:25:57 -0600

    Changed provider config  to be passed in from the caller module. The provider in the module config is just a proxy. This solves the import issues

--- autogen/auth.tf
+++ autogen/auth.tf
@@ -32,7 +32,5 @@ data "google_client_config" "default" {
  *****************************************/
 provider "kubernetes" {
   load_config_file       = false
-  host                   = "https://${local.cluster_endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
+  version = "~> 1.1"
 }
