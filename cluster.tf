9bb2f29923beeb88839f68300ed484ae75df729a
Julian V. Modesto <julian.modesto@liveramp.com>
2019-08-29 13:11:52 -0400

    Set cluster initial node count from var

--- cluster.tf
+++ cluster.tf
@@ -108,6 +108,7 @@ resource "google_container_cluster" "primary" {
     delete = "30m"
   }
 
+  initial_node_count = var.initial_node_count
   node_pool {
     name               = "default-pool"
     initial_node_count = var.initial_node_count
