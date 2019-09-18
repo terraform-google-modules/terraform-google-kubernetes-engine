9e2286dd818edfec54f81535722ac0dc9dd0af8f
pp <paul.palam@gmail.com>
2019-09-18 15:22:39 +0300

    Update integration tests to use new approach
     * Fix #245
     * Fixed lint-tests
     * Backwards compatibility of the `generate` function
     * Disabled kitchen test `stub-domains-private-local` (issue #264)

--- test/setup/make_source.sh
+++ test/setup/make_source.sh
@@ -1,4 +1,6 @@
-# Copyright 2018 Google LLC
+#!/usr/bin/env bash
+
+# Copyright 2019 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,3 +13,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+echo "#!/usr/bin/env bash" > ../source.sh
+
+project_id=$(terraform output project_id)
+echo "export TF_VAR_project_id='$project_id'" >> ../source.sh
+
+sa_json=$(terraform output sa_key)
+# shellcheck disable=SC2086
+echo "export SERVICE_ACCOUNT_JSON='$(echo $sa_json | base64 --decode)'" >> ../source.sh
+
+compute_engine_service_account=$(terraform output compute_engine_service_account)
+echo "export TF_VAR_compute_engine_service_account='$compute_engine_service_account'" >> ../source.sh
--- test/setup/outputs.tf
+++ test/setup/outputs.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2018 Google LLC
+ * Copyright 2019 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,3 +13,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+output "project_id" {
+  value = module.gke-project.project_id
+}
+
+output "sa_key" {
+  value     = google_service_account_key.int_test.private_key
+  sensitive = true
+}
+
+output "compute_engine_service_account" {
+  value = google_service_account.gke_sa.email
+}
