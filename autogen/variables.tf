55ade204c6eb74c522273f920939190a820cd831
Mirko Montanari <mmontan@google.com>
2019-09-30 17:56:10 -0700

    Add a parameter 'registry_project_id'
    The PR allows configuring the project holding the GCR registry when
    used in connection with 'create_service_account'=true and
    grant_registry_access=true.
    
    Holding the GCR is a project with other resources increases the risk of
    exposing sensitive data to the service account running the nodes, as the
    required permissions of role roles/storage.objectViewer provide access
    to all storage objects in the project.

--- autogen/variables.tf
+++ autogen/variables.tf
@@ -269,6 +269,12 @@ variable "grant_registry_access" {
   default     = false
 }
 
+variable "registry_project_id" {
+  type        = string
+  description = "Project holding the Google Container Registry. If empty, we use the cluster project. If grant_registry_access is true, storage.objectViewer role is assigned on this project."
+  default     = ""
+}
+
 variable "service_account" {
   type        = string
   description = "The service account to run nodes as if not overridden in `node_pools`. The create_service_account variable default value (true) will cause a cluster-specific service account to be created."
