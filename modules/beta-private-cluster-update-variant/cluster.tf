399f7857ffbc49759762efa044d6b02753257fa9
Kostas Evangelou <kostas.evangelou@revolut.com>
2019-10-16 17:33:00 +0100

    format tf files

--- modules/beta-private-cluster-update-variant/cluster.tf
+++ modules/beta-private-cluster-update-variant/cluster.tf
@@ -381,7 +381,7 @@ resource "google_container_node_pool" "pools" {
   }
 
   lifecycle {
-    ignore_changes = [initial_node_count]
+    ignore_changes        = [initial_node_count]
     create_before_destroy = true
   }
 
