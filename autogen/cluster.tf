642849264a42aa7351d18339304598e0a5fba09f
Devan Patel <devan2patel@gmail.com>
2019-10-31 11:07:13 +0000

    Add enable_shielded_nodes beta field

--- autogen/cluster.tf
+++ autogen/cluster.tf
@@ -65,6 +65,7 @@ resource "google_container_cluster" "primary" {
   enable_binary_authorization = var.enable_binary_authorization
   enable_intranode_visibility = var.enable_intranode_visibility
   default_max_pods_per_node   = var.default_max_pods_per_node
+  enable_shielded_nodes       = var.enable_shielded_nodes
 
   vertical_pod_autoscaling {
     enabled = var.enable_vertical_pod_autoscaling
