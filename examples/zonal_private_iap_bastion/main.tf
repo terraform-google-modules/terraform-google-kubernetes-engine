e0e42504ae9317f9c9df6329a3e331276a19dedd
Will Ripley <wripley@google.com>
2019-11-14 11:07:47 -0800

    Made changes to satisfy linting

--- examples/zonal_private_iap_bastion/main.tf
+++ examples/zonal_private_iap_bastion/main.tf
@@ -1,18 +1,18 @@
-/*
-Copyright 2019 Google LLC
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 module "gke" {
   source                  = "../../modules/beta-private-cluster"
@@ -46,13 +46,13 @@ module "gke" {
   issue_client_certificate = "false"
 
   master_authorized_networks_config = [
-    { 
+    {
       cidr_blocks = [
         {
           cidr_block   = module.gke-network.subnet_ips[0],
-          display_name = "internal subnet" 
+          display_name = "internal subnet"
         }
-      ] 
+      ]
     },
   ]
 
--- examples/zonal_private_iap_bastion/variables.tf
+++ examples/zonal_private_iap_bastion/variables.tf
@@ -1,18 +1,18 @@
-/*
-Copyright 2019 Google LLC
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 variable "cluster_name" {
   description = ""
@@ -25,7 +25,7 @@ variable "project_id" {
 
 variable "members" {
   // https://www.terraform.io/docs/configuration/variables.html#declaring-an-input-variable
-  type = list(string)
+  type        = list(string)
   description = "List of users, groups, SAs who need access to the bastion host"
 }
 
