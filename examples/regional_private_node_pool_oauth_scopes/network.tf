9b821bf7534d72cc0b83fe45548e5127ece59571
Alekhya Lakkadi <alekhyal@google.com>
2019-11-19 14:44:59 -0800

    addressed comments made for regional_private_node_pool example

--- examples/regional_private_node_pool_oauth_scopes/network.tf
+++ examples/regional_private_node_pool_oauth_scopes/network.tf
@@ -16,25 +16,26 @@
 
 module "gke-network" {
   source       = "terraform-google-modules/network/google"
+  version      = "~> 1.5"
   project_id   = var.project_id
-  network_name = var.network
+  network_name = "random-gke-network"
 
   subnets = [
     {
-      subnet_name   = var.subnet
+      subnet_name   = "random-gke-subnet"
       subnet_ip     = "10.0.0.0/24"
-      subnet_region = var.region
+      subnet_region = "us-west1"
     },
   ]
 
   secondary_ranges = {
-    "${var.subnet}" = [
+    "random-gke-subnet" = [
       {
-        range_name    = var.ip_range_pods
+        range_name    = "random-ip-range-pods"
         ip_cidr_range = "10.1.0.0/16"
       },
       {
-        range_name    = var.ip_range_services
+        range_name    = "random-ip-range-services"
         ip_cidr_range = "10.2.0.0/20"
       },
   ] }
