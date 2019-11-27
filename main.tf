4b559fd05b3e4df3cb06a4d078b68fcae75c0683
Morgante Pell <morgantep@google.com>
2019-11-27 17:56:24 -0500

    Fix terraform fmt

--- main.tf
+++ main.tf
@@ -83,7 +83,7 @@ locals {
 
 
   master_authorized_networks_config = length(var.master_authorized_networks) == 0 ? [] : [{
-    cidr_blocks: var.master_authorized_networks
+    cidr_blocks : var.master_authorized_networks
   }]
 
   cluster_output_node_pools_names    = concat(google_container_node_pool.pools.*.name, [""])
