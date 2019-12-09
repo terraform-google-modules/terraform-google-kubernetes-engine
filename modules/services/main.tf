cecdcddc8c1bf17dd2225f5048e038d4b3bbad1c
Morgante Pell <morgantep@google.com>
2019-12-09 14:38:33 -0500

    Pass through variables

--- modules/services/main.tf
+++ modules/services/main.tf
@@ -18,7 +18,10 @@ module "services" {
   source  = "terraform-google-modules/project-factory/google//modules/project_services"
   version = "~> 6.0.0"
 
-  project_id = var.project_id
+  project_id                  = var.project_id
+  enable_apis                 = var.enable_apis
+  disable_services_on_destroy = var.disable_services_on_destroy
+  disable_dependent_services  = var.disable_dependent_services
 
   activate_apis = [
     "compute.googleapis.com",
