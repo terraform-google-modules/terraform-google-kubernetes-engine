a5747874bc5c81256609451a92b7e3dcd53518ea
omazin <osmansimf@rambler.ru>
2019-12-09 19:50:45 +0300

    Make setting 'region' more clear. Workaround for #294, but the real issue's with terraform not able to override a default value when importing resources is there..

--- modules/beta-public-cluster/main.tf
+++ modules/beta-public-cluster/main.tf
@@ -34,7 +34,7 @@ resource "random_shuffle" "available_zones" {
 locals {
   // location
   location = var.regional ? var.region : var.zones[0]
-  region   = var.region == null ? join("-", slice(split("-", var.zones[0]), 0, 2)) : var.region
+  region   = var.regional ? var.region : join("-", slice(split("-", var.zones[0]), 0, 2))
   // for regional cluster - use var.zones if provided, use available otherwise, for zonal cluster use var.zones with first element extracted
   node_locations = var.regional ? coalescelist(compact(var.zones), sort(random_shuffle.available_zones.result)) : slice(var.zones, 1, length(var.zones))
   // kuberentes version
