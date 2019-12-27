b9e01034e43c3756ba9a21fee4cfb6a79671cb82
Gabriel Totusek <totusek@unity3d.com>
2019-12-26 22:26:20 -0800

    Regenerate modules and docs

--- modules/safer-cluster/variables.tf
+++ modules/safer-cluster/variables.tf
@@ -190,6 +190,18 @@ variable "upstream_nameservers" {
   default     = []
 }
 
+variable "logging_service" {
+  type        = string
+  description = "The logging service that the cluster should write logs to. Available options include logging.googleapis.com, logging.googleapis.com/kubernetes (beta), and none"
+  default     = "logging.googleapis.com"
+}
+
+variable "monitoring_service" {
+  type        = string
+  description = "The monitoring service that the cluster should write metrics to. Automatically send metrics from pods in the cluster to the Google Cloud Monitoring API. VM metrics will be collected by Google Compute Engine regardless of this setting Available options include monitoring.googleapis.com, monitoring.googleapis.com/kubernetes (beta) and none"
+  default     = "monitoring.googleapis.com"
+}
+
 variable "grant_registry_access" {
   type        = bool
   description = "Grants created cluster-specific service account storage.objectViewer role."
