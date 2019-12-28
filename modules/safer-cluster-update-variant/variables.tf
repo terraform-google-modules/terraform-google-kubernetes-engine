0487e5b6c839d8eff1c5b4b43a7592deaa71db79
omazin <osmansimf@rambler.ru>
2019-12-28 16:39:55 +0300

    [safer-cluster] Remove cluster_ipv4_cidr variable since it only works for routes-based clusters, where ip_allocation_policy is not defined.

--- modules/safer-cluster-update-variant/variables.tf
+++ modules/safer-cluster-update-variant/variables.tf
@@ -220,11 +220,6 @@ variable "service_account" {
   default     = ""
 }
 
-variable "cluster_ipv4_cidr" {
-  default     = ""
-  description = "The IP address range of the kubernetes pods in this cluster. Default is an automatically assigned CIDR."
-}
-
 variable "cluster_resource_labels" {
   type        = map(string)
   description = "The GCE resource labels (a map of key/value pairs) to be applied to the cluster"
