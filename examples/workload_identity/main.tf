da432142e998fb765b55136ce000ac8f05f9b2b7
Bharath KKB <bharathkrishnakb@gmail.com>
2020-02-06 22:03:08 -0600

    feat: Add new Workload identity [submodule](./modules/workload-identity) (#417)
    * initial workload identity addition
    
    various typo & linting fixes
    
    update README with output instead of hardcoded name
    
    update README to avoid cycle
    
    fix linting
    
    add missing outputs to fix linting
    
    update readme, generate docs
    
    fix linting for workload identity output
    
    updates from peer review
    
    add integration step for verifying workload identity
    
    initial workload identity integration test
    
    remove duplicate comment
    
    update dev tools version and just focus on workload identity tests here
    
    need these dependent steps
    
    project_ids is what we're using for variables
    
    add project_id output
    
    give registry_project_id a default in workload identity since we're not using it
    
    generate docs
    
    add the kubernetes provider
    
    rename cluster service account to work with this module
    
    bump google tf provider version
    
    newer gke clusters need to use kubernetes monitoring
    
    updated variable defaults in readme
    
    update monitoring variable in all modules
    
    update logging service variable too
    
    shorten some names to match identity account_id regex
    
    * accept incoming for conflicts on readme
    
    * fix WI config, fix tests
    
    * re enable all tests
    
    * fix vars,op
    
    * re enable cloud build for all tests
    
    * fix flake8 errors
    
    * increase timeout
    
    * add defaults, address comments
    
    Co-authored-by: Miles Matthias <miles.matthias@gmail.com>

---
+++ examples/workload_identity/main.tf
@@ -0,0 +1,65 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+locals {
+  cluster_type = "regional"
+}
+
+provider "google" {
+  version = "~> 2.20.1"
+  region  = var.region
+}
+
+provider "kubernetes" {
+  version                = "~> 1.10"
+  host                   = module.gke.endpoint
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
+
+module "gke" {
+  source                   = "../../modules/beta-public-cluster/"
+  project_id               = var.project_id
+  name                     = "${local.cluster_type}-cluster${var.cluster_name_suffix}"
+  region                   = var.region
+  network                  = var.network
+  subnetwork               = var.subnetwork
+  ip_range_pods            = var.ip_range_pods
+  ip_range_services        = var.ip_range_services
+  remove_default_node_pool = true
+  service_account          = "create"
+  identity_namespace       = "${var.project_id}.svc.id.goog"
+  node_metadata            = "GKE_METADATA_SERVER"
+  node_pools = [
+    {
+      name         = "wi-pool"
+      min_count    = 1
+      max_count    = 2
+      auto_upgrade = true
+    }
+  ]
+}
+
+module "workload_identity" {
+  source              = "../../modules/workload-identity"
+  project_id          = var.project_id
+  name                = "iden-${module.gke.name}"
+  namespace           = "default"
+  use_existing_k8s_sa = false
+}
+
+data "google_client_config" "default" {
+}
