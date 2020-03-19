418d9b30863ed67638bef87602de3910169e1195
Eric Horst <erich@quavy.com>
2020-03-19 06:45:34 -0700

    fix: update and pin kubernetes provider to >= 1.11.1 (#453)
    * update and pin kubernetes provider to >= 1.11.1
    
    * a more enlightened version constraint
    
    * version spec tf will grok
    
    * version spec meeting all needs
    
    Co-authored-by: Eric UW <erich-uw@macky.local>

--- examples/workload_identity/main.tf
+++ examples/workload_identity/main.tf
@@ -24,7 +24,7 @@ provider "google" {
 }
 
 provider "kubernetes" {
-  version                = "~> 1.10"
+  version                = "~> 1.10, != 1.11.0"
   host                   = module.gke.endpoint
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(module.gke.ca_certificate)
