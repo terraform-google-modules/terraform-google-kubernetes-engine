c3ec15a97fd8a6a436e7b291fa684a935861a7ac
Josef Brandl <mail@josefbrandl.de>
2020-03-23 11:30:37 +0100

    Simplify value of kubernetes provider host param (issue #461)

--- examples/deploy_service/main.tf
+++ examples/deploy_service/main.tf
@@ -25,7 +25,7 @@ provider "google" {
 
 provider "kubernetes" {
   load_config_file       = false
-  host                   = "https://${module.gke.endpoint}"
+  host                   = module.gke.endpoint
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(module.gke.ca_certificate)
 }
