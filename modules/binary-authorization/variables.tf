cf5ab5a7cde3e656d7ce0d702e3dff2be482f024
Syed I Nayer <imrannayer@users.noreply.github.com>
2020-06-27 13:01:45 -0500

    updated variable type to bool

--- modules/binary-authorization/variables.tf
+++ modules/binary-authorization/variables.tf
@@ -37,12 +37,12 @@ variable crypto-algorithm {
 
 variable "disable_services_on_destroy" {
   description = "Whether project services will be disabled when the resources are destroyed. https://www.terraform.io/docs/providers/google/r/google_project_service.html#disable_on_destroy"
-  default     = "false"
-  type        = string
+  default     = false
+  type        = bool
 }
 
 variable "disable_dependent_services" {
   description = "Whether services that are enabled and which depend on this service should also be disabled when this service is destroyed. https://www.terraform.io/docs/providers/google/r/google_project_service.html#disable_dependent_services"
-  default     = "false"
-  type        = string
+  default     = false
+  type        = bool
 }
