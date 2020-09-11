701b54a182cfcf547218856a889759d687f57ae1
cloud-pharaoh <aminamansour@gmail.com>
2020-09-10 19:45:49 -0700

    Change membership name to expected name

--- examples/simple_zonal_with_asm/hub.tf
+++ examples/simple_zonal_with_asm/hub.tf
@@ -15,9 +15,10 @@
  */
 
 module "hub" {
-  source           = "../../modules/hub"
-  project_id       = var.project_id
-  location         = module.gke.location
-  cluster_name     = module.gke.name
-  cluster_endpoint = module.gke.endpoint
+  source                  = "../../modules/hub"
+  project_id              = var.project_id
+  location                = module.gke.location
+  cluster_name            = module.gke.name
+  cluster_endpoint        = module.gke.endpoint
+  gke_hub_membership_name = "gke-asm-membership"
 }
