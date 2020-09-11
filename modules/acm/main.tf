e5ec95a621eea49200239138616f66cd73721ef1
cloud-pharaoh <aminamansour@gmail.com>
2020-09-10 22:24:16 -0700

    expose service account variable for ACM module (#667)

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -35,6 +35,7 @@ module "acm_operator" {
   source_format            = var.source_format
   hierarchy_controller     = var.hierarchy_controller
   enable_log_denies        = var.enable_log_denies
+  service_account_key_file = var.service_account_key_file
 
   operator_latest_manifest_url  = "gs://config-management-release/released/latest/config-management-operator.yaml"
   operator_cr_template_path     = "${path.module}/templates/acm-config.yml.tpl"
