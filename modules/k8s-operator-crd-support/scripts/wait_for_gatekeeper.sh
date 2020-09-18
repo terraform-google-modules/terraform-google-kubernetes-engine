318f6d7f3f1fe9d59c3964cda9ed5f213e3ffe94
Amina Mansour <aminamansour@gmail.com>
2020-09-18 00:50:53 -0700

    use deployment ready

--- modules/k8s-operator-crd-support/scripts/wait_for_gatekeeper.sh
+++ modules/k8s-operator-crd-support/scripts/wait_for_gatekeeper.sh
@@ -12,4 +12,31 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-kubectl wait --for=condition=available --timeout=600s deployment --all -n gatekeeper-system
+
+is_deployment_ready() {
+    kubectl --context $1 -n $2 get deploy $3 &> /dev/null
+    export exit_code=$?
+    while [ ! " ${exit_code} " -eq 0 ]
+        do
+            sleep 5
+            echo -e "Waiting for deployment $3 in cluster $1 to be created..."
+            kubectl --context $1 -n $2 get deploy $3 &> /dev/null
+            export exit_code=$?
+        done
+    echo -e "Deployment $3 in cluster $1 created."
+
+    # Once deployment is created, check for deployment status.availableReplicas is greater than 0
+    export availableReplicas=$(kubectl --context $1 -n $2 get deploy $3 -o json | jq -r '.status.availableReplicas')
+    while [[ " ${availableReplicas} " == " null " ]]
+        do
+            sleep 5
+            echo -e "Waiting for deployment $3 in cluster $1 to become ready..."
+            export availableReplicas=$(kubectl --context $1 -n $2 get deploy $3 -o json | jq -r '.status.availableReplicas')
+        done
+
+    echo -e "$3 in cluster $1 is ready with replicas ${availableReplicas}."
+    return ${availableReplicas}
+}
+
+# Gatekeeper causes issues if not ready
+is_deployment_ready ${GKE_CTX} gatekeeper-system gatekeeper-controller-manager
