561b9dcd7352dc87fbadb420abc04f1f06952c91
Amina Mansour <aminamansour@gmail.com>
2020-09-22 22:32:33 -0700

    shell check

--- modules/k8s-operator-crd-support/scripts/wait_for_gatekeeper.sh
+++ modules/k8s-operator-crd-support/scripts/wait_for_gatekeeper.sh
@@ -14,51 +14,51 @@
 # limitations under the License.
 
 is_deployment_ready() {
-    kubectl --context $1 -n $2 get deploy $3 &> /dev/null
+    kubectl --context "$1" -n "$2" get deploy "$3" &> /dev/null
     export exit_code=$?
     while [ ! " ${exit_code} " -eq 0 ]
         do
             sleep 5
             echo -e "Waiting for deployment $3 in cluster $1 to be created..."
-            kubectl --context $1 -n $2 get deploy $3 &> /dev/null
+            kubectl --context "$1" -n "$2" get deploy "$3" &> /dev/null
             export exit_code=$?
         done
     echo -e "Deployment $3 in cluster $1 created."
 
     # Once deployment is created, check for deployment status.availableReplicas is greater than 0
-    export availableReplicas=$(kubectl --context $1 -n $2 get deploy $3 -o json | jq -r '.status.availableReplicas')
+    availableReplicas=$(kubectl --context "$1" -n "$2" get deploy "$3" -o json | jq -r '.status.availableReplicas')
     while [[ " ${availableReplicas} " == " null " ]]
         do
             sleep 5
             echo -e "Waiting for deployment $3 in cluster $1 to become ready..."
-            export availableReplicas=$(kubectl --context $1 -n $2 get deploy $3 -o json | jq -r '.status.availableReplicas')
+            availableReplicas=$(kubectl --context "$1" -n "$2" get deploy "$3" -o json | jq -r '.status.availableReplicas')
         done
 
     echo -e "$3 in cluster $1 is ready with replicas ${availableReplicas}."
-    return ${availableReplicas}
+    return "${availableReplicas}"
 }
 
 is_service_ready() {
-    kubectl --context $1 -n $2 get service $3 &> /dev/null
+    kubectl --context "$1" -n "$2" get service "$3" &> /dev/null
     export exit_code=$?
     while [ ! " ${exit_code} " -eq 0 ]
         do
             sleep 5
             echo -e "Waiting for service $3 in cluster $1 to be created..."
-            kubectl --context $1 -n $2 get service $3 &> /dev/null
+            kubectl --context "$1" -n "$2" get service "$3" &> /dev/null
             export exit_code=$?
         done
     echo -e "Service $3 in cluster $1 created."
 
     # Once service is created, check endpoints is greater than 0
-    kubectl --context $1 -n $2 get endpoints $3
+    kubectl --context "$1" -n "$2" get endpoints "$3"
     export exit_code=$?
 
     while [ ! " ${exit_code} " -eq 0 ]
         do
             sleep 5
             echo -e "Waiting for endpoints for service $3 in cluster $1 to become ready..."
-            kubectl --context $1 -n $2 get endpoints $3
+            kubectl --context "$1" -n "$2" get endpoints "$3"
             export exit_code=$?
         done
 
