c7ec8077146a233f36e558708bd9a99819e903f4
marko7460 <vlahovic@google.com>
2020-10-09 13:11:35 -0700

    feat: add node_pool_taints to all the modules (#705)

--- modules/private-cluster/cluster.tf
+++ modules/private-cluster/cluster.tf
@@ -211,6 +211,17 @@ resource "google_container_node_pool" "pools" {
         "disable-legacy-endpoints" = var.disable_legacy_metadata_endpoints
       },
     )
+    dynamic "taint" {
+      for_each = concat(
+        local.node_pools_taints["all"],
+        local.node_pools_taints[each.value["name"]],
+      )
+      content {
+        effect = taint.value.effect
+        key    = taint.value.key
+        value  = taint.value.value
+      }
+    }
     tags = concat(
       lookup(local.node_pools_tags, "default_values", [true, true])[0] ? [local.cluster_network_tag] : [],
       lookup(local.node_pools_tags, "default_values", [true, true])[1] ? ["${local.cluster_network_tag}-${each.value["name"]}"] : [],
