12d452b9700fa3a94a6641ebf48747e6c7a81862
Dev <Dev25@users.noreply.github.com>
2020-11-02 16:55:56 +0000

    Set var type to string
    Signed-off-by: Dev <Dev25@users.noreply.github.com>

--- variables.tf
+++ variables.tf
@@ -415,6 +415,7 @@ variable "gcloud_upgrade" {
 
 
 variable "impersonate_service_account" {
+  type        = string
   description = "An optional service account to impersonate for gcloud commands. If this service account is not specified, the module will use Application Default Credentials."
   default     = ""
 }
