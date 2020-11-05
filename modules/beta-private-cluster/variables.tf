1544adc36509c5917179fb01877ba9f7e050abde
nguyenhoaibao <nhbao.la@gmail.com>
2020-11-05 23:05:38 +0700

    feat: Add cluster_telemetry var to beta submodules (#728)

--- modules/beta-private-cluster/variables.tf
+++ modules/beta-private-cluster/variables.tf
@@ -293,6 +293,12 @@ variable "configure_ip_masq" {
   default     = false
 }
 
+variable "cluster_telemetry_type" {
+  type        = string
+  description = "Available options include ENABLED, DISABLED, and SYSTEM_ONLY"
+  default     = null
+}
+
 variable "logging_service" {
   type        = string
   description = "The logging service that the cluster should write logs to. Available options include logging.googleapis.com, logging.googleapis.com/kubernetes (beta), and none"
--- modules/beta-public-cluster-update-variant/cluster.tf
+++ modules/beta-public-cluster-update-variant/cluster.tf
@@ -56,8 +56,14 @@ resource "google_container_cluster" "primary" {
   }
   min_master_version = var.release_channel != null ? null : local.master_version
 
-  logging_service    = var.logging_service
-  monitoring_service = var.monitoring_service
+  dynamic "cluster_telemetry" {
+    for_each = local.cluster_telemetry_type_is_set ? [1] : []
+    content {
+      type = var.cluster_telemetry_type
+    }
+  }
+  logging_service    = local.cluster_telemetry_type_is_set ? null : var.logging_service
+  monitoring_service = local.cluster_telemetry_type_is_set ? null : var.monitoring_service
 
   cluster_autoscaling {
     enabled             = var.cluster_autoscaling.enabled
