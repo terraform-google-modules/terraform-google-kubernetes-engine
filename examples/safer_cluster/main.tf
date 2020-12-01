99ca8a71677a4c3f451f68e1c176fecefb7817fd
Jan Nabbefeld <jan@odise.de>
2020-12-01 16:58:16 +0100

    fix: terraform fmt

--- examples/safer_cluster/main.tf
+++ examples/safer_cluster/main.tf
@@ -62,7 +62,7 @@ module "gke" {
   istio    = true
   cloudrun = true
 
-  notification_config_topic = google_pubsub_topic.updates.id
+  notification_config_topic  = google_pubsub_topic.updates.id
   enable_notification_config = true
 }
 
