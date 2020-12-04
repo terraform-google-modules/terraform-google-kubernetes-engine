42a9588f04066f7838b85ec206fccaf3c06aced2
rwong2888 <61470342+rwong2888@users.noreply.github.com>
2020-12-04 13:18:01 -0500

    fix: Remove max Terraform version constraint, allowing 0.14 compatibility (#757)

--- examples/simple_zonal_with_asm/versions.tf
+++ examples/simple_zonal_with_asm/versions.tf
@@ -15,5 +15,5 @@
  */
 
 terraform {
-  required_version = ">=0.12, <0.14"
+  required_version = ">=0.12"
 }
--- modules/private-cluster/versions.tf
+++ modules/private-cluster/versions.tf
@@ -15,7 +15,7 @@
  */
 
 terraform {
-  required_version = ">=0.12.6, <0.14"
+  required_version = ">=0.12.6"
 
   required_providers {
     google = ">= 3.39.0, <4.0.0"
