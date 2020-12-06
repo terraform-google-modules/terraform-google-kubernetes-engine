af3be0879efa67c1ff79ccbd4890fb96ab7955d9
descampsk <kevindescamps78@gmail.com>
2020-12-06 20:27:07 +0100

    feat: Enable vertical autoscaling in GA modules (#758)

--- modules/beta-private-cluster/cluster.tf
+++ modules/beta-private-cluster/cluster.tf
@@ -78,16 +78,16 @@ resource "google_container_cluster" "primary" {
     }
   }
 
+  vertical_pod_autoscaling {
+    enabled = var.enable_vertical_pod_autoscaling
+  }
+
   default_max_pods_per_node = var.default_max_pods_per_node
 
   enable_shielded_nodes       = var.enable_shielded_nodes
   enable_binary_authorization = var.enable_binary_authorization
   enable_intranode_visibility = var.enable_intranode_visibility
   enable_kubernetes_alpha     = var.enable_kubernetes_alpha
-
-  vertical_pod_autoscaling {
-    enabled = var.enable_vertical_pod_autoscaling
-  }
 
   dynamic "pod_security_policy_config" {
     for_each = var.enable_pod_security_policy ? [var.enable_pod_security_policy] : []
