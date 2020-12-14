f019ea48c0c4b7b8b62ff8429577f1801da16c8e
Jai Govindani <jai@honestbank.com>
2020-12-14 10:32:16 +0700

    fix(beta-private-cluster-update-variant): version constraint deprecation

--- versions.tf
+++ versions.tf
@@ -18,6 +18,7 @@ terraform {
   required_version = ">=0.12.6"
 
   required_providers {
-    google = ">= 3.39.0, <4.0.0"
+    google     = ">= 3.39.0, <4.0.0"
+    kubernetes = "~> 1.10, != 1.11.0"
   }
 }
