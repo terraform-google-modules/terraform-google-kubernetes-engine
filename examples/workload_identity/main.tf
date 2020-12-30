093679e74edd929126353e761b182293aa75c614
Raj <the.raj.saxena@gmail.com>
2020-12-30 17:37:00 +0530

    Move version constraint to required_providers block

--- examples/workload_identity/main.tf
+++ examples/workload_identity/main.tf
@@ -24,7 +24,6 @@ provider "google" {
 }
 
 provider "kubernetes" {
-  version                = "~> 1.10, != 1.11.0"
   host                   = module.gke.endpoint
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(module.gke.ca_certificate)
