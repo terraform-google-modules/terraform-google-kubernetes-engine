6a29e6246de05cc01d518eecc15c9e1c21cb8ba9
Abhinav Rau <abhinavrau@users.noreply.github.com>
2021-01-28 11:47:34 -0500

    feat: Hub registration using kubeconfig and labels support (#785)
    * initial support for using kubecontext for hub registration
    
    * Tested with GKE using kubectl
    
    * Tested with GKE using kubectl
    
    * Fixed typo in README
    
    * Fixed formatting errors
    
    * Fixed formatting errors
    
    * Fixed formatting and renamed example to be more consistent
    
    * Fixed formatting and renamed example to be more consistent
    
    * remove test files
    
    * fixed typo in README
    
    * use a flag to switch gke vs kubeconfig instead of having two scripts
    
    * Update modules/hub/scripts/gke_hub_registration.sh
    
    specify PROJECT_ID to be more specific
    
    Co-authored-by: Bharath KKB <bharathkrishnakb@gmail.com>
    
    * Update modules/hub/scripts/gke_hub_unregister.sh
    
    fix number of arguments check
    
    Co-authored-by: Bharath KKB <bharathkrishnakb@gmail.com>
    
    * update simple_zonal_with_hub_kubeconfig to use kind cluster
    
    Co-authored-by: Abhinav Rau <arau@google.com>
    Co-authored-by: Bharath KKB <bharathkrishnakb@gmail.com>
    Co-authored-by: Morgante Pell <morgantep@google.com>

---
+++ examples/simple_zonal_with_hub_kubeconfig/hub.tf
@@ -0,0 +1,27 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+module "hub" {
+  source                  = "../../modules/hub"
+  project_id              = var.project_id
+  location                = "remote"
+  cluster_name            = kind_cluster.test-cluster.name
+  cluster_endpoint        = kind_cluster.test-cluster.endpoint
+  gke_hub_membership_name = kind_cluster.test-cluster.name
+  gke_hub_sa_name         = "sa-for-kind-cluster-membership"
+  use_kubeconfig          = true
+  labels                  = "testlabel=usekubecontext"
+}
