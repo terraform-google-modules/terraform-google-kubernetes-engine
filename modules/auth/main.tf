523dc449e6dc77867920eee963ce93d871bc5520
Florian Strübe <f.struebe@gmx.de>
2021-03-05 17:10:56 +0100

    Return private_endpoint if explicitly requested, otherwise return default endpoint

--- modules/auth/main.tf
+++ modules/auth/main.tf
@@ -17,9 +17,8 @@
 locals {
   cluster_ca_certificate = data.google_container_cluster.gke_cluster.master_auth != null ? data.google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate : ""
   private_endpoint       = try(data.google_container_cluster.gke_cluster.private_cluster_config[0].private_endpoint, "")
-  public_endpoint        = try(data.google_container_cluster.gke_cluster.private_cluster_config[0].public_endpoint, "")
   default_endpoint       = data.google_container_cluster.gke_cluster.endpoint != null ? data.google_container_cluster.gke_cluster.endpoint : ""
-  endpoint               = var.use_private_endpoint == true ? local.private_endpoint : local.public_endpoint != "" ? local.public_endpoint : local.default_endpoint
+  endpoint               = var.use_private_endpoint == true ? local.private_endpoint : local.default_endpoint
   host                   = local.endpoint != "" ? "https://${local.endpoint}" : ""
   context                = data.google_container_cluster.gke_cluster.name != null ? data.google_container_cluster.gke_cluster.name : ""
 }
