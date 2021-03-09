c20a6aad1cd5b3e26b76f8d361004026e8ab3b00
James Duncan <JamesDuncanNz@users.noreply.github.com>
2021-03-08 20:43:26 -0500

    feat!: Added support for multi-project GKE Hub registration (#840)
    * Added support for multi-project GKE Hub registration
    
    Added an optional variable HUB_PROJECT_ID which will allow you to specific a seperate
    GCP project for the GKE HUB than the project the cluster is deployed to.
    
    This included updating the 3 examples the leveraged the hub module.
    
    Issue: #837
    
    * Adding service idenity resource for multi project deployments
    
    Ensuring that the Hub default Service Account exists
    when adding a cluster from outside the hub project
    
    Issue: #837
    
    * Fixing bharathkkb comments
    
    Fixes #837
    
    * Fix linting issue
    
    * Removed Google project data souce
    
    Issue: #837
    
    * Adding upgrade documentation for this change.
    
    * Lint updates on readme.
    
    Co-authored-by: James Duncan <jdun@google.com>
    Co-authored-by: Bharath KKB <bharathkrishnakb@gmail.com>

--- modules/hub/scripts/gke_hub_registration.sh
+++ modules/hub/scripts/gke_hub_registration.sh
@@ -22,11 +22,10 @@ fi
 
 GKE_CLUSTER_FLAG=$1
 MEMBERSHIP_NAME=$2
-CLUSTER_LOCATION=$3
-CLUSTER_NAME=$4
-SERVICE_ACCOUNT_KEY=$5
-PROJECT_ID=$6
-LABELS=$7
+SERVICE_ACCOUNT_KEY=$3
+CLUSTER_URI=$4
+HUB_PROJECT_ID=$5
+LABELS=$6
 
 #write temp key, cleanup at exit
 tmp_file=$(mktemp)
@@ -37,12 +36,12 @@ echo "${SERVICE_ACCOUNT_KEY}" | base64 ${B64_ARG} > "$tmp_file"
 
 if [[ ${GKE_CLUSTER_FLAG} == 1 ]]; then
     echo "Registering GKE Cluster."
-    gcloud container hub memberships register "${MEMBERSHIP_NAME}" --gke-cluster="${CLUSTER_LOCATION}"/"${CLUSTER_NAME}" --service-account-key-file="${tmp_file}" --project="${PROJECT_ID}" --quiet
+    gcloud container hub memberships register "${MEMBERSHIP_NAME}" --gke-uri="${CLUSTER_URI}" --service-account-key-file="${tmp_file}" --project="${HUB_PROJECT_ID}" --quiet
 else
     echo "Registering a non-GKE Cluster. Using current-context to register Hub membership."
     #Get the kubeconfig
     CONTEXT=$(kubectl config current-context)
-    gcloud container hub memberships register "${MEMBERSHIP_NAME}" --context="${CONTEXT}" --service-account-key-file="${tmp_file}" --project="${PROJECT_ID}" --quiet
+    gcloud container hub memberships register "${MEMBERSHIP_NAME}" --context="${CONTEXT}" --service-account-key-file="${tmp_file}" --project="${HUB_PROJECT_ID}" --quiet
 fi
 
 
@@ -50,5 +49,5 @@ fi
 if [ -z ${LABELS+x} ]; then
     echo "No hub labels to apply."
 else
-    gcloud container hub memberships update "${MEMBERSHIP_NAME}" --update-labels "$LABELS" --project="${PROJECT_ID}"
+    gcloud container hub memberships update "${MEMBERSHIP_NAME}" --update-labels "$LABELS" --project="${HUB_PROJECT_ID}"
 fi
--- modules/hub/scripts/gke_hub_unregister.sh
+++ modules/hub/scripts/gke_hub_unregister.sh
@@ -15,25 +15,22 @@
 
 set -e
 
-if [ "$#" -lt 5 ]; then
+if [ "$#" -lt 4 ]; then
     >&2 echo "Not all expected arguments set."
     exit 1
 fi
 
 GKE_CLUSTER_FLAG=$1
 MEMBERSHIP_NAME=$2
-CLUSTER_LOCATION=$3
-CLUSTER_NAME=$4
-PROJECT_ID=$5
-
-
+CLUSTER_URI=$3
+HUB_PROJECT_ID=$4
 
 if [[ ${GKE_CLUSTER_FLAG} == 1 ]]; then
     echo "Un-Registering GKE Cluster."
-    gcloud container hub memberships unregister "${MEMBERSHIP_NAME}" --gke-cluster="${CLUSTER_LOCATION}"/"${CLUSTER_NAME}" --project "${PROJECT_ID}"
+    gcloud container hub memberships unregister "${MEMBERSHIP_NAME}" --gke-uri="${CLUSTER_URI}" --project "${HUB_PROJECT_ID}"
 else
     echo "Un-Registering a non-GKE Cluster. Using current-context to unregister Hub membership."
     #Get Current context
     CONTEXT=$(kubectl config current-context)
-    gcloud container hub memberships unregister "${MEMBERSHIP_NAME}" --context="${CONTEXT}" --project="${PROJECT_ID}"
+    gcloud container hub memberships unregister "${MEMBERSHIP_NAME}" --context="${CONTEXT}" --project="${HUB_PROJECT_ID}"
 fi
