c9f75b329033056f0575ca815fd3a46fd3791c39
Zhengzhe Yang <zhengzhey@google.com>
2021-08-03 23:25:35 +0000

    add enable_namespace_creation flag and start testing with 1.10

--- examples/simple_zonal_with_asm/main.tf
+++ examples/simple_zonal_with_asm/main.tf
@@ -67,15 +67,16 @@ module "gke" {
 }
 
 module "asm" {
-  source                = "../../modules/asm"
-  cluster_name          = module.gke.name
-  cluster_endpoint      = module.gke.endpoint
-  project_id            = var.project_id
-  location              = module.gke.location
-  enable_cluster_roles  = true
-  enable_cluster_labels = true
-  enable_gcp_apis       = true
-  enable_gcp_components = true
-  options               = ["envoy-access-log"]
-  outdir                = "./${module.gke.name}-outdir"
+  source                    = "../../modules/asm"
+  cluster_name              = module.gke.name
+  cluster_endpoint          = module.gke.endpoint
+  project_id                = var.project_id
+  location                  = module.gke.location
+  enable_cluster_roles      = true
+  enable_cluster_labels     = true
+  enable_gcp_apis           = true
+  enable_gcp_components     = true
+  enable_namespace_creation = true
+  options                   = ["envoy-access-log"]
+  outdir                    = "./${module.gke.name}-outdir"
 }
--- modules/asm/main.tf
+++ modules/asm/main.tf
@@ -97,6 +97,6 @@ module "asm_install" {
   service_account_key_file    = var.service_account_key_file
   impersonate_service_account = var.impersonate_service_account
 
-  kubectl_create_command  = "${path.module}/scripts/install_asm.sh ${var.project_id} ${var.cluster_name} ${var.location} ${var.asm_version} ${var.mode} ${var.managed_control_plane} ${var.skip_validation} ${local.options_string} ${local.custom_overlays_string} ${var.enable_all} ${var.enable_cluster_roles} ${var.enable_cluster_labels} ${var.enable_gcp_components} ${var.enable_registration} ${var.outdir} ${var.ca} ${local.ca_cert} ${local.ca_key} ${local.root_cert} ${local.cert_chain} ${local.service_account_string} ${local.key_file_string} ${local.asm_git_tag_string} ${local.revision_name_string}"
+  kubectl_create_command  = "${path.module}/scripts/install_asm.sh ${var.project_id} ${var.cluster_name} ${var.location} ${var.asm_version} ${var.mode} ${var.managed_control_plane} ${var.skip_validation} ${local.options_string} ${local.custom_overlays_string} ${var.enable_all} ${var.enable_cluster_roles} ${var.enable_cluster_labels} ${var.enable_gcp_components} ${var.enable_registration} ${var.enable_namespace_creation} ${var.outdir} ${var.ca} ${local.ca_cert} ${local.ca_key} ${local.root_cert} ${local.cert_chain} ${local.service_account_string} ${local.key_file_string} ${local.asm_git_tag_string} ${local.revision_name_string}"
   kubectl_destroy_command = "${path.module}/scripts/destroy_asm.sh"
 }
