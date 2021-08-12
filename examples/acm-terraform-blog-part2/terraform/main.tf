54345a34a059f48603e43f4eaad8d1da272c3fd8
Alex Bulankou <alexb@yojowa.com>
2021-08-11 17:20:12 -0700

    Addressing comments

--- examples/acm-terraform-blog-part2/terraform/main.tf
+++ examples/acm-terraform-blog-part2/terraform/main.tf
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 terraform {
   required_providers {
@@ -49,6 +64,11 @@ resource "google_gke_hub_feature_membership" "feature_member" {
         policy_dir  = var.policy_dir
         secret_type = "none"
       }
+    }
+    policy_controller {
+      enabled                    = true
+      template_library_installed = true
+      referential_rules_enabled  = true
     }
   }
 }
