bc41a98a7264e7b5055be23e2cde00069f7b8848
Alex Bulankou <alexb@yojowa.com>
2021-09-02 12:24:52 -0700

    chore: Adding the code for Part3 of the ACM GKE blog - enabling Config Connector and initializing GCP resources (#980)

---
+++ examples/acm-terraform-blog-part3/config-root/cis-k8s-1.5.1/README.md
@@ -0,0 +1,6 @@
+CIS Kubernetes Benchmark v.1.5.1
+==================================================
+
+## Description
+
+These constraints are intended to address the recommendations the CIS Kubernetes Benchmark v.1.5.1. They have not been certified by CIS.
---
+++ examples/acm-terraform-blog-part3/terraform/gke.tf
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+module "enabled_google_apis" {
+  source  = "terraform-google-modules/project-factory/google//modules/project_services"
+  version = "~> 10.0"
+
+  project_id                  = var.project
+  disable_services_on_destroy = false
+
+  activate_apis = [
+    "compute.googleapis.com",
+    "container.googleapis.com",
+    "gkehub.googleapis.com",
+    "anthosconfigmanagement.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "sqladmin.googleapis.com"
+  ]
+}
+
+module "gke" {
+  source             = "terraform-google-modules/kubernetes-engine/google//modules/beta-public-cluster"
+  version            = "~> 16.0"
+  project_id         = module.enabled_google_apis.project_id
+  name               = "sfl-acm-part3"
+  region             = var.region
+  zones              = [var.zone]
+  initial_node_count = 4
+  network            = "default"
+  subnetwork         = "default"
+  ip_range_pods      = ""
+  ip_range_services  = ""
+  config_connector   = true
+}
+
+module "wi" {
+  source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  version             = "~> 16.0.1"
+  gcp_sa_name         = "cnrmsa"
+  cluster_name        = module.gke.name
+  name                = "cnrm-controller-manager"
+  location            = var.zone
+  use_existing_k8s_sa = true
+  annotate_k8s_sa     = false
+  namespace           = "cnrm-system"
+  project_id          = module.enabled_google_apis.project_id
+  roles               = ["roles/owner"]
+}
