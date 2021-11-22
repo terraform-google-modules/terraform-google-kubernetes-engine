190ae7f41d73f2e6803e3b8fd64160a781042501
Jack Whelpton <jack.whelpton@rakuten.com>
2021-11-22 11:35:21 -0800

    Updates network module for Google provider 4.0 compatibility

--- examples/regional_private_node_pool_oauth_scopes/network.tf
+++ examples/regional_private_node_pool_oauth_scopes/network.tf
@@ -16,7 +16,7 @@
 
 module "gke-network" {
   source       = "terraform-google-modules/network/google"
-  version      = "~> 2.5"
+  version      = ">= 4.0.1, < 5.0.0"
   project_id   = var.project_id
   network_name = "random-gke-network"
 
--- examples/safer_cluster_iap_bastion/network.tf
+++ examples/safer_cluster_iap_bastion/network.tf
@@ -17,7 +17,7 @@
 
 module "vpc" {
   source  = "terraform-google-modules/network/google"
-  version = "~> 2.5"
+  version = ">= 4.0.1, < 5.0.0"
 
   project_id   = module.enabled_google_apis.project_id
   network_name = var.network_name
