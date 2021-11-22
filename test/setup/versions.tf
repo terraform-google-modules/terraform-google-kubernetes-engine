fc4abaf5900c0a498b480934fbccd98b0c9c5bd0
Jack Whelpton <jack.whelpton@rakuten.com>
2021-11-22 11:00:57 -0800

    Updates tests to use latest Google provider
    * addresses warning about multiple provider blocks

--- test/setup/versions.tf
+++ test/setup/versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "3.50.0"
+      version = "4.1.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "3.50.0"
+      version = "4.1.0"
     }
     random = {
       source = "hashicorp/random"
