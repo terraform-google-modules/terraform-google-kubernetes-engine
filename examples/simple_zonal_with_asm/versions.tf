e4d372dc15dbe0dc28f2f96d7c4b3b0d78adef87
Jack Whelpton <jack.whelpton@rakuten.com>
2021-11-24 23:12:20 -0800

    Updates pinned version of Google provider for example

--- examples/simple_zonal_with_asm/versions.tf
+++ examples/simple_zonal_with_asm/versions.tf
@@ -19,7 +19,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 3.63.0"
+      version = "~> 4.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
