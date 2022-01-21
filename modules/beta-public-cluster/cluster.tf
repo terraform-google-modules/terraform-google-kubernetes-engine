1d459d7d71f2403aa753a359b14bfff5b99c1db0
Adrian Chifor <ac@matterlabs.dev>
2022-01-21 12:00:23 +0000

    Add spot vm support to beta clusters

--- modules/beta-public-cluster/cluster.tf
+++ modules/beta-public-cluster/cluster.tf
@@ -444,6 +444,7 @@ resource "google_container_node_pool" "pools" {
       local.service_account,
     )
     preemptible = lookup(each.value, "preemptible", false)
+    spot = lookup(each.value, "spot", false)
 
     oauth_scopes = concat(
       local.node_pools_oauth_scopes["all"],
