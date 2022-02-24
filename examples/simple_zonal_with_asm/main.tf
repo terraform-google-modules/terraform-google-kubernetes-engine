d46465a6677ff3b8512fe13c5c15d60c501c5a8e
Sam Naser <samnaser@google.com>
2022-02-24 12:26:22 -0800

    update CROSS_CLUSTER_SERVICE_DISCOVERY to multicluster_mode

--- examples/simple_zonal_with_asm/main.tf
+++ examples/simple_zonal_with_asm/main.tf
@@ -57,10 +57,10 @@ module "gke" {
 }
 
 module "asm" {
-  source                                 = "../../modules/asm"
-  project_id                             = var.project_id
-  cluster_name                           = module.gke.name
-  cluster_location                       = module.gke.location
-  enable_cross_cluster_service_discovery = true
-  enable_cni                             = true
+  source            = "../../modules/asm"
+  project_id        = var.project_id
+  cluster_name      = module.gke.name
+  cluster_location  = module.gke.location
+  multicluster_mode = "connected"
+  enable_cni        = true
 }
