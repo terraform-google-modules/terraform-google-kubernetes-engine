0ff6967c10ad64fcc192a2c8cf2e91aea009d707
Thomas Weiss <tweiss@maisonsdumonde.com>
2022-04-01 14:39:10 +0200

    fixup! Changes after review

--- modules/private-cluster-update-variant/cluster.tf
+++ modules/private-cluster-update-variant/cluster.tf
@@ -62,7 +62,7 @@ resource "google_container_cluster" "primary" {
       content {
         service_account = local.service_account
         oauth_scopes    = local.node_pools_oauth_scopes["all"]
-        image_type      = lookup(var.node_pools[0], "image_type", null)
+        image_type      = lookup(var.node_pools[0], "image_type", "COS_CONTAINERD")
       }
     }
     dynamic "resource_limits" {
