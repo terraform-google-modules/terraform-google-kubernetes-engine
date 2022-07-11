c5a2fc1667b0b2f0817de66cef18b6f8e33c7d00
drfaust92 <ilia.lazebnik@gmail.com>
2022-07-11 16:22:33 +0300

    revert network_config

--- cluster.tf
+++ cluster.tf
@@ -297,12 +297,6 @@ resource "google_container_node_pool" "pools" {
     }
   }
 
-  dynamic "network_config" {
-    for_each = length(lookup(each.value, "pod_range", "")) > 0 ? [each.value] : []
-    content {
-      pod_range = lookup(network_config.value, "pod_range", null)
-    }
-  }
 
   management {
     auto_repair  = lookup(each.value, "auto_repair", true)
