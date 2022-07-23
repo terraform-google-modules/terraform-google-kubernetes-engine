4bf0011202667b6efe8a0b5aea2910f1c3250968
Ilia Lazebnik <Ilia.lazebnik@gmail.com>
2022-07-23 06:08:24 +0300

    feat: cloud-dns support (#1317)
    * cloud-dns support
    
    * fix
    
    * lint
    
    * autopilot
    
    Co-authored-by: Bharath KKB <bharathkrishnakb@gmail.com>

--- cluster.tf
+++ cluster.tf
@@ -161,6 +161,15 @@ resource "google_container_cluster" "primary" {
     ignore_changes = [node_pool, initial_node_count, resource_labels["asmv"], resource_labels["mesh_id"]]
   }
 
+  dynamic "dns_config" {
+    for_each = var.cluster_dns_provider == "CLOUD_DNS" ? [1] : []
+    content {
+      cluster_dns        = var.cluster_dns_provider
+      cluster_dns_scope  = var.cluster_dns_scope
+      cluster_dns_domain = var.cluster_dns_domain
+    }
+  }
+
   timeouts {
     create = lookup(var.timeouts, "create", "45m")
     update = lookup(var.timeouts, "update", "45m")
