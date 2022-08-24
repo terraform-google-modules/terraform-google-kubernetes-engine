4f76d52d98e00f2a0b463c024715f6dcc88cf2d3
Alex <alex.wang19@gmail.com>
2022-08-24 14:38:27 -0500

    Remove trailing whitespace

--- modules/asm/main.tf
+++ modules/asm/main.tf
@@ -61,7 +61,7 @@ module "cpr" {
   cluster_name             = var.cluster_name
   cluster_location         = var.cluster_location
   internal_ip              = var.internal_ip
-  service_account_key_file = var.service_account_key_file	
+  service_account_key_file = var.service_account_key_file
   
   kubectl_create_command  = "${path.module}/scripts/create_cpr.sh ${local.revision_name} ${local.channel} ${var.enable_cni} ${var.enable_vpc_sc}"
   kubectl_destroy_command = "${path.module}/scripts/destroy_cpr.sh ${local.revision_name}"
