45cd6082803721446f52bad91e8f49839b60d6d4
Ubuntu <mkpi@QBY-01.2svxuh0oexkexj2mkebn452anh.ax.internal.cloudapp.net>
2022-11-02 12:54:57 +0000

    fixed GitHub Issue #1445 | location_policy permadrifting

--- cluster.tf
+++ cluster.tf
@@ -331,8 +331,9 @@ resource "google_container_node_pool" "pools" {
   dynamic "autoscaling" {
     for_each = lookup(each.value, "autoscaling", true) ? [each.value] : []
     content {
-      min_node_count = lookup(autoscaling.value, "min_count", 1)
-      max_node_count = lookup(autoscaling.value, "max_count", 100)
+      location_policy = "BALANCED"
+      min_node_count  = lookup(autoscaling.value, "min_count", 1)
+      max_node_count  = lookup(autoscaling.value, "max_count", 100)
     }
   }
 
