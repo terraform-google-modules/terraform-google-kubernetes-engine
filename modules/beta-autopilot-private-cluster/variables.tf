625b869766661840a7733dd8331e7ebe34e5f459
田端大樹 <butterthon@gmail.com>
2022-11-20 17:53:52 +0900

    Fixed error in CI check

--- modules/beta-autopilot-private-cluster/variables.tf
+++ modules/beta-autopilot-private-cluster/variables.tf
@@ -96,6 +96,12 @@ variable "http_load_balancing" {
   default     = true
 }
 
+variable "filestore_csi_driver" {
+  type        = bool
+  description = "Enable filestore csi addon"
+  default     = false
+}
+
 variable "service_external_ips" {
   type        = bool
   description = "Whether external ips specified by a service will be allowed in this cluster"
