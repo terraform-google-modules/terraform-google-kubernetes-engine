adadcece26b10296d92509d1026269da8da93f80
田端大樹 <butterthon@gmail.com>
2022-11-20 18:09:41 +0900

    run `make build`

--- modules/beta-autopilot-private-cluster/cluster.tf
+++ modules/beta-autopilot-private-cluster/cluster.tf
@@ -110,9 +110,6 @@ resource "google_container_cluster" "primary" {
       disabled = !var.horizontal_pod_autoscaling
     }
 
-    gcp_filestore_csi_driver_config {
-      enabled = var.filestore_csi_driver
-    }
 
   }
 
