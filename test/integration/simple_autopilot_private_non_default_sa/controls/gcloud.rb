265e64ddcba86a0e0730aa383d4e1bee53756071
Marco Ferrari <ferrari.marco@gmail.com>
2022-12-09 12:25:08 +0000

    Set the provided SA when creating autopilot clusters

---
+++ test/integration/simple_autopilot_private_non_default_sa/controls/gcloud.rb
@@ -0,0 +1,72 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+project_id = attribute('project_id')
+location = attribute('location')
+cluster_name = attribute('cluster_name')
+
+control "gcloud" do
+  title "Google Compute Engine GKE configuration"
+  describe command("gcloud --project=#{project_id} container clusters --zone=#{location} describe #{cluster_name} --format=json") do
+    its(:exit_status) { should eq 0 }
+    its(:stderr) { should eq '' }
+
+    let!(:data) do
+      if subject.exit_status == 0
+        JSON.parse(subject.stdout)
+      else
+        {}
+      end
+    end
+
+    describe "cluster" do
+      it "is running" do
+        expect(data['status']).to eq('RUNNING').or eq('RECONCILING')
+      end
+
+      it "is autopilot" do
+        expect(data['autopilot']['enabled']).to eq true
+      end
+
+      it "doesn't use the default service account" do
+        expect(data['autoscaling']['serviceAccount']).not_to eq "default"
+      end
+
+      it "is regional" do
+        expect(data['location']).to match(/^.*[1-9]$/)
+      end
+
+      it "uses the private endpoint" do
+        expect(data['privateClusterConfig']['enablePrivateEndpoint']).to eq true
+      end
+
+      it "uses private nodes" do
+        expect(data['privateClusterConfig']['enablePrivateNodes']).to eq true
+      end
+
+      it "has the expected addon settings" do
+        expect(data['addonsConfig']).to include(
+          "horizontalPodAutoscaling" => {},
+          "httpLoadBalancing" => {},
+          "kubernetesDashboard" => {
+            "disabled" => true,
+          },
+          "networkPolicyConfig" => {
+            "disabled" => true,
+          },
+        )
+      end
+    end
+  end
+end
