4773d55f523222fdc595a4778d45a8b98e2493f7
Vojtěch Šplíchal <splichal@gmail.com>
2022-12-30 12:26:59 +0100

    autogen

--- modules/beta-private-cluster-update-variant/cluster.tf
+++ modules/beta-private-cluster-update-variant/cluster.tf
@@ -568,7 +568,8 @@ resource "google_container_node_pool" "pools" {
   dynamic "network_config" {
     for_each = length(lookup(each.value, "pod_range", "")) > 0 ? [each.value] : []
     content {
-      pod_range = lookup(network_config.value, "pod_range", null)
+      pod_range            = lookup(network_config.value, "pod_range", null)
+      enable_private_nodes = var.enable_private_nodes
     }
   }
 
@@ -779,7 +780,8 @@ resource "google_container_node_pool" "windows_pools" {
   dynamic "network_config" {
     for_each = length(lookup(each.value, "pod_range", "")) > 0 ? [each.value] : []
     content {
-      pod_range = lookup(network_config.value, "pod_range", null)
+      pod_range            = lookup(network_config.value, "pod_range", null)
+      enable_private_nodes = var.enable_private_nodes
     }
   }
 
