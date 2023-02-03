536d2bcaf961d777525db0aa771ff849e5949b5c
Gari Singh <gari.r.singh@gmail.com>
2023-02-03 14:25:12 -0500

    fix: remove datapath provider from Autopilot modules (#1556)
    Signed-off-by: Gari Singh <gari.r.singh@gmail.com>
    Co-authored-by: Bharath KKB <bharathkrishnakb@gmail.com>

--- modules/beta-autopilot-private-cluster/cluster.tf
+++ modules/beta-autopilot-private-cluster/cluster.tf
@@ -119,8 +119,6 @@ resource "google_container_cluster" "primary" {
 
   }
 
-  datapath_provider = var.datapath_provider
-
   networking_mode = "VPC_NATIVE"
   ip_allocation_policy {
     cluster_secondary_range_name  = var.ip_range_pods
