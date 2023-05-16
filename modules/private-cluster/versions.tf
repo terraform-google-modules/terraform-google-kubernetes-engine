fbf6373e6dbd854cafbbe71d7c579ae9801c5c2e
Andrew Peabody <andrewpeabody@google.com>
2023-05-16 12:44:18 -0700

    fix: avoid TPG 4.65.0 and 4.65.1 (#1637)

--- modules/private-cluster/versions.tf
+++ modules/private-cluster/versions.tf
@@ -21,7 +21,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.51.0, < 5.0"
+      version = ">= 4.51.0, < 4.65.0, !=4.65.0, !=4.65.1"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
