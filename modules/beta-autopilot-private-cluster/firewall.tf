7acceda663a4004feb8fe8bc2070f3df034d6f8a
Edvin Norling <edvin.norling@kognic.com>
2023-05-25 11:05:41 +0200

    Set max firewall name to 36
    This to be able to have longer and unique names.
    The firewall API supports 64 charters
    Solves #1527
    
    Signed-off-by: Edvin Norling <edvin.norling@kognic.com>

--- modules/beta-autopilot-private-cluster/firewall.tf
+++ modules/beta-autopilot-private-cluster/firewall.tf
@@ -26,7 +26,7 @@
  *****************************************/
 resource "google_compute_firewall" "intra_egress" {
   count       = var.add_cluster_firewall_rules ? 1 : 0
-  name        = "gke-${substr(var.name, 0, min(25, length(var.name)))}-intra-cluster-egress"
+  name        = "gke-${substr(var.name, 0, min(36, length(var.name)))}-intra-cluster-egress"
   description = "Managed by terraform gke module: Allow pods to communicate with each other and the master"
   project     = local.network_project_id
   network     = var.network
@@ -64,7 +64,7 @@ resource "google_compute_firewall" "intra_egress" {
  *****************************************/
 resource "google_compute_firewall" "tpu_egress" {
   count       = var.add_cluster_firewall_rules && var.enable_tpu ? 1 : 0
-  name        = "gke-${substr(var.name, 0, min(25, length(var.name)))}-tpu-egress"
+  name        = "gke-${substr(var.name, 0, min(36, length(var.name)))}-tpu-egress"
   description = "Managed by terraform gke module: Allow pods to communicate with TPUs"
   project     = local.network_project_id
   network     = var.network
@@ -93,7 +93,7 @@ resource "google_compute_firewall" "tpu_egress" {
  *****************************************/
 resource "google_compute_firewall" "master_webhooks" {
   count       = var.add_cluster_firewall_rules || var.add_master_webhook_firewall_rules ? 1 : 0
-  name        = "gke-${substr(var.name, 0, min(25, length(var.name)))}-webhooks"
+  name        = "gke-${substr(var.name, 0, min(36, length(var.name)))}-webhooks"
   description = "Managed by terraform gke module: Allow master to hit pods for admission controllers/webhooks"
   project     = local.network_project_id
   network     = var.network
@@ -120,7 +120,7 @@ resource "google_compute_firewall" "master_webhooks" {
 resource "google_compute_firewall" "shadow_allow_pods" {
   count = var.add_shadow_firewall_rules ? 1 : 0
 
-  name        = "gke-shadow-${substr(var.name, 0, min(25, length(var.name)))}-all"
+  name        = "gke-shadow-${substr(var.name, 0, min(36, length(var.name)))}-all"
   description = "Managed by terraform gke module: A shadow firewall rule to match the default rule allowing pod communication."
   project     = local.network_project_id
   network     = var.network
@@ -149,7 +149,7 @@ resource "google_compute_firewall" "shadow_allow_pods" {
 resource "google_compute_firewall" "shadow_allow_master" {
   count = var.add_shadow_firewall_rules ? 1 : 0
 
-  name        = "gke-shadow-${substr(var.name, 0, min(25, length(var.name)))}-master"
+  name        = "gke-shadow-${substr(var.name, 0, min(36, length(var.name)))}-master"
   description = "Managed by terraform GKE module: A shadow firewall rule to match the default rule allowing worker nodes communication."
   project     = local.network_project_id
   network     = var.network
@@ -175,7 +175,7 @@ resource "google_compute_firewall" "shadow_allow_master" {
 resource "google_compute_firewall" "shadow_allow_nodes" {
   count = var.add_shadow_firewall_rules ? 1 : 0
 
-  name        = "gke-shadow-${substr(var.name, 0, min(25, length(var.name)))}-vms"
+  name        = "gke-shadow-${substr(var.name, 0, min(36, length(var.name)))}-vms"
   description = "Managed by Terraform GKE module: A shadow firewall rule to match the default rule allowing worker nodes communication."
   project     = local.network_project_id
   network     = var.network
@@ -210,7 +210,7 @@ resource "google_compute_firewall" "shadow_allow_nodes" {
 resource "google_compute_firewall" "shadow_allow_inkubelet" {
   count = var.add_shadow_firewall_rules ? 1 : 0
 
-  name        = "gke-shadow-${substr(var.name, 0, min(25, length(var.name)))}-inkubelet"
+  name        = "gke-shadow-${substr(var.name, 0, min(36, length(var.name)))}-inkubelet"
   description = "Managed by terraform GKE module: A shadow firewall rule to match the default rule allowing worker nodes & pods communication to kubelet."
   project     = local.network_project_id
   network     = var.network
@@ -237,7 +237,7 @@ resource "google_compute_firewall" "shadow_allow_inkubelet" {
 resource "google_compute_firewall" "shadow_deny_exkubelet" {
   count = var.add_shadow_firewall_rules ? 1 : 0
 
-  name        = "gke-shadow-${substr(var.name, 0, min(25, length(var.name)))}-exkubelet"
+  name        = "gke-shadow-${substr(var.name, 0, min(36, length(var.name)))}-exkubelet"
   description = "Managed by terraform GKE module: A shadow firewall rule to match the default deny rule to kubelet."
   project     = local.network_project_id
   network     = var.network
