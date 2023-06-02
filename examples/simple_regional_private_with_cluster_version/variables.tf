66f0c78a63a38b2e247bd4528b37ea5509661d29
slimatic <salim.31@gmail.com>
2023-06-02 12:59:15 -0400

    feat(cluster.tf): add support to set initial release channel version (#1625)
    Co-authored-by: Andrew Peabody <andrewpeabody@google.com>

--- examples/simple_regional_private_with_cluster_version/variables.tf
+++ examples/simple_regional_private_with_cluster_version/variables.tf
@@ -14,30 +14,37 @@
  * limitations under the License.
  */
 
-variable "project_ids" {
-  type        = list(string)
-  description = "The GCP projects to use for integration tests"
+variable "project_id" {
+  description = "The project ID to host the cluster in"
+}
+
+variable "cluster_name_suffix" {
+  description = "A suffix to append to the default cluster name"
+  default     = ""
 }
 
 variable "region" {
-  description = "The GCP region to create and test resources in"
-  default     = "us-central1"
+  description = "The region to host the cluster in"
 }
 
-variable "zones" {
-  type        = list(string)
-  description = "The GCP zones to create and test resources in, for applicable tests"
-  default     = ["us-central1-a", "us-central1-b", "us-central1-c"]
+variable "network" {
+  description = "The VPC network to host the cluster in"
 }
 
-variable "compute_engine_service_accounts" {
-  type        = list(string)
-  description = "The email addresses of the service account to associate with the GKE cluster"
+variable "subnetwork" {
+  description = "The subnetwork to host the cluster in"
 }
 
-variable "registry_project_ids" {
-  description = "Projects to use for granting access to GCR registries, if requested"
-  type        = list(string)
+variable "ip_range_pods" {
+  description = "The secondary ip range to use for pods"
+}
+
+variable "ip_range_services" {
+  description = "The secondary ip range to use for services"
+}
+
+variable "compute_engine_service_account" {
+  description = "Service account to associate to the nodes in the cluster"
 }
 
 variable "kubernetes_version" {
