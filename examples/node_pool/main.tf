3f27bc20bf5ee81ccda862307fa8c9624357ec93
Andrew Peabody <andrewpeabody@google.com>
2023-09-12 17:46:16 -0700

    chore: fix CI test sequencing and machine type (#1735)

--- examples/node_pool/main.tf
+++ examples/node_pool/main.tf
@@ -50,18 +50,17 @@ module "gke" {
       auto_upgrade    = true
     },
     {
-      name               = "pool-02"
-      machine_type       = "a2-highgpu-1g"
-      min_count          = 1
-      max_count          = 2
-      local_ssd_count    = 0
-      disk_size_gb       = 30
-      disk_type          = "pd-standard"
-      accelerator_count  = 1
-      accelerator_type   = "nvidia-tesla-a100"
-      gpu_partition_size = "1g.5gb"
-      auto_repair        = false
-      service_account    = var.compute_engine_service_account
+      name              = "pool-02"
+      machine_type      = "n1-standard-2"
+      min_count         = 1
+      max_count         = 2
+      local_ssd_count   = 0
+      disk_size_gb      = 30
+      disk_type         = "pd-standard"
+      accelerator_count = 1
+      accelerator_type  = "nvidia-tesla-p4"
+      auto_repair       = false
+      service_account   = var.compute_engine_service_account
     },
     {
       name               = "pool-03"
