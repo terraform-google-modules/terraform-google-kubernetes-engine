ae26016a913077295c88551888e5f5cec970f7a9
Eric Zhao <zhaoe@google.com>
2023-09-27 01:31:41 +1000

    feat!: make promethus configurable (#1715)

--- modules/private-cluster/cluster.tf
+++ modules/private-cluster/cluster.tf
@@ -82,17 +82,11 @@ resource "google_container_cluster" "primary" {
   }
   monitoring_service = local.logmon_config_is_set ? null : var.monitoring_service
   dynamic "monitoring_config" {
-    for_each = length(var.monitoring_enabled_components) > 0 || var.monitoring_enable_managed_prometheus ? [1] : []
-
+    for_each = local.logmon_config_is_set || local.logmon_config_is_set ? [1] : []
     content {
-      enable_components = length(var.monitoring_enabled_components) > 0 ? var.monitoring_enabled_components : []
-
-      dynamic "managed_prometheus" {
-        for_each = var.monitoring_enable_managed_prometheus ? [1] : []
-
-        content {
-          enabled = var.monitoring_enable_managed_prometheus
-        }
+      enable_components = var.monitoring_enabled_components
+      managed_prometheus {
+        enabled = var.monitoring_enable_managed_prometheus
       }
     }
   }
