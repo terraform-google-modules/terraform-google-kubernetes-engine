aa8a6d4d758b898f85a5294354ecddddc2591b96
Ross Bannerman <ross.bannerman@qoria.com>
2023-11-14 19:44:01 -0500

    feat: Add support for configuring allow_net_admin in autopilot clusters (#1768)

--- modules/beta-autopilot-private-cluster/cluster.tf
+++ modules/beta-autopilot-private-cluster/cluster.tf
@@ -130,6 +130,8 @@ resource "google_container_cluster" "primary" {
 
   }
 
+  allow_net_admin = var.allow_net_admin
+
   networking_mode = "VPC_NATIVE"
 
   protect_config {
--- modules/beta-autopilot-public-cluster/variables.tf
+++ modules/beta-autopilot-public-cluster/variables.tf
@@ -418,3 +418,8 @@ variable "timeouts" {
   }
 }
 
+variable "allow_net_admin" {
+  description = "(Optional) Enable NET_ADMIN for the cluster."
+  type        = bool
+  default     = null
+}
