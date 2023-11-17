434c3a77e6c7a8b9be18ac418cb1a41fe1c9a27e
Shubham Singh <skshubham@google.com>
2023-11-17 18:03:31 +0530

    feat: add support for pod_range in private cluster

--- modules/private-cluster/cluster.tf
+++ modules/private-cluster/cluster.tf
@@ -455,7 +455,6 @@ resource "google_container_node_pool" "pools" {
     }
   }
 
-
   management {
     auto_repair  = lookup(each.value, "auto_repair", true)
     auto_upgrade = lookup(each.value, "auto_upgrade", local.default_auto_upgrade)
