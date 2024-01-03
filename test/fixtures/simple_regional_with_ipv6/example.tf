d6cb39062cc6ecc2f86af463afd883d1fd780657
Ghaleb Al-Habian <ghabian@gmail.com>
2024-01-03 11:27:47 -0500

    feat: dual stack (IPV4_IPV6) support (#1818)
    Co-authored-by: Jonathan Meyers <jmeyers2@gmail.com>
    Co-authored-by: Jonathan Meyers <jmymy@users.noreply.github.com>
    Co-authored-by: Bharath KKB <bharathkrishnakb@gmail.com>
    Co-authored-by: Ghaleb <glb@google.com>
    Co-authored-by: Ghaleb Al-Habian <galhabian@google.com>
    Co-authored-by: Andrew Peabody <andrewpeabody@google.com>

---
+++ test/fixtures/simple_regional_with_ipv6/example.tf
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+locals {
+  cluster_index = 1
+}
+
+module "example" {
+  source = "../../../examples/simple_regional_with_ipv6"
+
+  project_id                     = var.project_ids[local.cluster_index]
+  cluster_name_suffix            = "-${random_string.suffix.result}"
+  region                         = var.region
+  network                        = google_compute_network.main.name
+  subnetwork                     = google_compute_subnetwork.main.name
+  ip_range_pods                  = google_compute_subnetwork.main.secondary_ip_range[0].range_name
+  ip_range_services              = google_compute_subnetwork.main.secondary_ip_range[1].range_name
+  stack_type                     = "IPV4_IPV6"
+  compute_engine_service_account = var.compute_engine_service_accounts[local.cluster_index]
+
+}
