f3b650feed47a42ca586a13aa6a74a43bb626fb6
Andrew Peabody <andrewpeabody@google.com>
2024-02-02 12:35:13 -0800

    feat: add enable_private_nodes options to node_pool network_config without pod_range

--- modules/private-cluster-update-variant/cluster.tf
+++ modules/private-cluster-update-variant/cluster.tf
@@ -552,7 +552,7 @@ resource "google_container_node_pool" "pools" {
   }
 
   dynamic "network_config" {
-    for_each = length(lookup(each.value, "pod_range", "")) > 0 ? [each.value] : []
+    for_each = length(lookup(each.value, "pod_range", "")) + length(lookup(each.value, "enable_private_nodes", "")) > 0 ? [each.value] : []
     content {
       pod_range            = lookup(network_config.value, "pod_range", null)
       enable_private_nodes = var.enable_private_nodes
@@ -762,7 +762,7 @@ resource "google_container_node_pool" "windows_pools" {
   }
 
   dynamic "network_config" {
-    for_each = length(lookup(each.value, "pod_range", "")) > 0 ? [each.value] : []
+    for_each = length(lookup(each.value, "pod_range", "")) + length(lookup(each.value, "enable_private_nodes", "")) > 0 ? [each.value] : []
     content {
       pod_range            = lookup(network_config.value, "pod_range", null)
       enable_private_nodes = var.enable_private_nodes
