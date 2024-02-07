2f735a3629ed8ea472ea893fab47992e59077981
Andrew Peabody <andrewpeabody@google.com>
2024-02-07 15:34:31 -0800

    Update kms.tf

--- examples/safer_cluster_iap_bastion/kms.tf
+++ examples/safer_cluster_iap_bastion/kms.tf
@@ -16,7 +16,7 @@
 
 module "kms" {
   source          = "terraform-google-modules/kms/google"
-  version         = "~> 2.3.0"
+  version         = "~> 2.3"
   project_id      = var.project_id
   location        = var.region
   keyring         = "gke-keyring"
