6003f1e2004df37b48841e808a1c85f3232286c0
Georgia Zoi Leng <58770450+GorginZ@users.noreply.github.com>
2024-04-14 18:14:12 +1000

    Update examples/autopilot_private_firewalls/network.tf
    use current module version
    
    Co-authored-by: Andrew Peabody <andrewpeabody@google.com>

--- examples/autopilot_private_firewalls/network.tf
+++ examples/autopilot_private_firewalls/network.tf
@@ -16,7 +16,7 @@
 
 module "gcp-network" {
   source  = "terraform-google-modules/network/google"
-  version = ">= 4.0.1"
+  version = ">= 9.0.0"
 
   project_id   = var.project_id
   network_name = local.network_name
