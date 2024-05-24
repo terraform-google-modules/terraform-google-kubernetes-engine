1bc271b51b6da55bcaa72db442ef0dd8c0203ed0
Jim Gustavsson <40056436+jimgus@users.noreply.github.com>
2024-05-24 19:34:51 +0200

    feat: add support for gpu_sharing_config on nodepool (#1874)

--- cluster.tf
+++ cluster.tf
@@ -644,6 +644,14 @@ resource "google_container_node_pool" "pools" {
             gpu_driver_version = lookup(each.value, "gpu_driver_version", "")
           }
         }
+
+        dynamic "gpu_sharing_config" {
+          for_each = lookup(each.value, "gpu_sharing_strategy", "") != "" ? [1] : []
+          content {
+            gpu_sharing_strategy       = lookup(each.value, "gpu_sharing_strategy", "")
+            max_shared_clients_per_gpu = lookup(each.value, "max_shared_clients_per_gpu", 2)
+          }
+        }
       }
     }
 
@@ -880,6 +888,14 @@ resource "google_container_node_pool" "windows_pools" {
           for_each = lookup(each.value, "gpu_driver_version", "") != "" ? [1] : []
           content {
             gpu_driver_version = lookup(each.value, "gpu_driver_version", "")
+          }
+        }
+
+        dynamic "gpu_sharing_config" {
+          for_each = lookup(each.value, "gpu_sharing_strategy", "") != "" ? [1] : []
+          content {
+            gpu_sharing_strategy       = lookup(each.value, "gpu_sharing_strategy", "")
+            max_shared_clients_per_gpu = lookup(each.value, "max_shared_clients_per_gpu", 2)
           }
         }
       }
