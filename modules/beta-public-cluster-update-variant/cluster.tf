36904e18322766de52146e00428831d59efb9490
drfaust92 <ilia.lazebnik@gmail.com>
2024-08-02 20:32:42 -0400

    support reservation affinity

--- modules/beta-public-cluster-update-variant/cluster.tf
+++ modules/beta-public-cluster-update-variant/cluster.tf
@@ -747,7 +747,7 @@ resource "google_container_node_pool" "pools" {
       content {
         consume_reservation_type = reservation_affinity.value.consume_reservation_type
         key                      = reservation_affinity.value.key
-        values                   = split(",", reservation_affinity.value.values)
+        values                   = reservation_affinity.value.values == null ? null : split(",", reservation_affinity.value.values)
       }
     }
     labels = merge(
@@ -1051,7 +1051,7 @@ resource "google_container_node_pool" "windows_pools" {
       content {
         consume_reservation_type = reservation_affinity.value.consume_reservation_type
         key                      = reservation_affinity.value.key
-        values                   = split(",", reservation_affinity.value.values)
+        values                   = reservation_affinity.value.values == null ? null : split(",", reservation_affinity.value.values)
       }
     }
     labels = merge(
--- modules/private-cluster-update-variant/cluster.tf
+++ modules/private-cluster-update-variant/cluster.tf
@@ -704,7 +704,7 @@ resource "google_container_node_pool" "pools" {
       content {
         consume_reservation_type = reservation_affinity.value.consume_reservation_type
         key                      = reservation_affinity.value.key
-        values                   = split(",", reservation_affinity.value.values)
+        values                   = reservation_affinity.value.values == null ? null : split(",", reservation_affinity.value.values)
       }
     }
     labels = merge(
@@ -995,7 +995,7 @@ resource "google_container_node_pool" "windows_pools" {
       content {
         consume_reservation_type = reservation_affinity.value.consume_reservation_type
         key                      = reservation_affinity.value.key
-        values                   = split(",", reservation_affinity.value.values)
+        values                   = reservation_affinity.value.values == null ? null : split(",", reservation_affinity.value.values)
       }
     }
     labels = merge(
