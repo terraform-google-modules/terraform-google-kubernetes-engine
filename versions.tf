83e57988b211b1e94e40b973c439f28dee8e2f17
William Yardley <wyardley@users.noreply.github.com>
2024-09-24 17:50:33 -0700

    update version specification order

--- versions.tf
+++ versions.tf
@@ -21,7 +21,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 5.44.1, < 7, !=6.0.0, !=6.0.1, !=6.1.0, !=6.2.0, !=6.3.0"
+      version = ">= 5.44.1, !=6.0.0, !=6.0.1, !=6.1.0, !=6.2.0, !=6.3.0, < 7"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
