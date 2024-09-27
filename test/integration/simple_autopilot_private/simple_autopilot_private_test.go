6e5559e4e72aa9f3ac105e65f9dc329d50a2eaf1
Andrew Peabody <andrewpeabody@google.com>
2024-09-27 09:45:37 -0700

    chore(CI): enable simple_autopilot_private_test DefaultVerify (#2091)

--- test/integration/simple_autopilot_private/simple_autopilot_private_test.go
+++ test/integration/simple_autopilot_private/simple_autopilot_private_test.go
@@ -30,8 +30,7 @@ func TestSimpleAutopilotPrivate(t *testing.T) {
 	)
 
 	bpt.DefineVerify(func(assert *assert.Assertions) {
-		//Skipping Default Verify as the Verify Stage fails due to change in Client Cert Token
-		// bpt.DefaultVerify(assert)
+		bpt.DefaultVerify(assert)
 
 		projectId := bpt.GetStringOutput("project_id")
 		location := bpt.GetStringOutput("location")
