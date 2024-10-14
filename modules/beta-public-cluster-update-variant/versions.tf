b6c5fa6d083abc02bb05d6a993e8af93552e7907
William Yardley <wyardley@users.noreply.github.com>
2024-10-14 12:14:39 -0700

    update required versions

--- modules/beta-public-cluster-update-variant/versions.tf
+++ modules/beta-public-cluster-update-variant/versions.tf
@@ -21,11 +21,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 5.44.1, !=6.0.0, !=6.0.1, !=6.1.0, !=6.2.0, !=6.3.0, < 7"
+      version = ">= 5.44.2, !=6.0.0, !=6.0.1, !=6.1.0, !=6.2.0, !=6.3.0, !=6.4.0, !=6.5.0, !=6.6.0, < 7"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 5.44.1, !=6.0.0, !=6.0.1, !=6.1.0, !=6.2.0, !=6.3.0, < 7"
+      version = ">= 5.44.2, !=6.0.0, !=6.0.1, !=6.1.0, !=6.2.0, !=6.3.0, !=6.4.0, !=6.5.0, !=6.6.0, < 7"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
