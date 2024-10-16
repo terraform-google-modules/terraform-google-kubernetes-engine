87637f2721b07d2c657ee9cdff52f647fc0caa7b
redoak666 <ningleity@gmail.com>
2024-10-16 15:56:36 -0500

    correct confidential node field name

--- cluster.tf
+++ cluster.tf
@@ -782,7 +782,7 @@ resource "google_container_node_pool" "pools" {
     }
 
     dynamic "confidential_nodes" {
-      for_each = lookup(each.value, "enable_confidential_nodes", null) != null ? [each.value.confidential_nodes] : []
+      for_each = lookup(each.value, "enable_confidential_nodes", null) != null ? [each.value.enable_confidential_nodes] : []
       content {
         enabled = confidential_nodes.value
       }
