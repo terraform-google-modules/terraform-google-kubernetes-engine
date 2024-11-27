030fe31db7abe99f839b792a6e1223e24ef517f4
Andrew Peabody <andrewpeabody@google.com>
2024-11-27 01:14:34 +0000

    fix: enable TPGv6.11+

--- modules/private-cluster/versions.tf
+++ modules/private-cluster/versions.tf
@@ -21,7 +21,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 6.7.0, < 6.11.0"
+      version = ">= 6.7.0, < 7"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
