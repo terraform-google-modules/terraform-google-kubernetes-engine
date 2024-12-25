b2b35e24ce32a1a4c57f6051ca6e67d7d3408750
harvey lin <harveylin324@gmail.com>
2024-12-24 21:31:34 -0700

    Add lifecycle ignore for kubelet_config in google_container_node_pool

--- cluster.tf
+++ cluster.tf
@@ -833,8 +833,7 @@ resource "google_container_node_pool" "pools" {
   }
 
   lifecycle {
-    ignore_changes = [initial_node_count]
-
+    ignore_changes = [initial_node_count, node_config.0.kubelet_config]
   }
 
   timeouts {
