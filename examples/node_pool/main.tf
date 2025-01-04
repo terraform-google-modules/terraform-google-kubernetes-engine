c629490fba04ba59ac723b5d0389a98a459dbf59
drfaust92 <ilia.lazebnik@gmail.com>
2025-01-04 17:52:02 -0500

    align tests
    Signed-off-by: drfaust92 <ilia.lazebnik@gmail.com>

--- examples/node_pool/main.tf
+++ examples/node_pool/main.tf
@@ -94,7 +94,7 @@ module "gke" {
     },
     {
       name                         = "pool-05"
-      machine_type                 = "n1-standard-2"
+      machine_type                 = "c3-standard-4"
       node_count                   = 1
       enable_nested_virtualization = true
     },
@@ -164,7 +164,6 @@ module "gke" {
   }
 
   node_pools_hugepage_size_1g = {
-    all     = "1"
-    pool-03 = "2"
+    pool-05 = "2"
   }
 }
