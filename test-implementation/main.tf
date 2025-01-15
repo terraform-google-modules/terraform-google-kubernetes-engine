c591bb3e6b69a7f59a8a06b9b75594078cf62ab2
samuelarogbonlo <sbayo971@gmail.com>
2025-01-15 22:28:05 +0100

    style: fix terraform formatting

--- test-implementation/main.tf
+++ test-implementation/main.tf
@@ -1,11 +1,11 @@
 module "gke" {
   source                        = "../modules/beta-private-cluster"
   project_id                    = "rare-style-447407-s1"
-  name                         = "test-custom-role"
-  region                       = "us-central1"
-  network                      = "default"
-  subnetwork                   = "default"
-  ip_range_pods                = "gke-pods"
-  ip_range_services            = "gke-services"
+  name                          = "test-custom-role"
+  region                        = "us-central1"
+  network                       = "default"
+  subnetwork                    = "default"
+  ip_range_pods                 = "gke-pods"
+  ip_range_services             = "gke-services"
   monitoring_metric_writer_role = "projects/rare-style-447407-s1/roles/custom_metrics_writer"
 }
