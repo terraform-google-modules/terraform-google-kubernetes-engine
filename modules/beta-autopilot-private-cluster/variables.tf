de1cf9b07555b08ec71a2fc58acb1cfbba9da1d8
Alex Sonn <alex@sonn.co.il>
2025-02-10 11:25:21 +0200

    fix(cgroup): Make value empty by default instead of null

--- modules/beta-autopilot-private-cluster/variables.tf
+++ modules/beta-autopilot-private-cluster/variables.tf
@@ -329,10 +329,10 @@ variable "node_pools_cgroup_mode" {
   type        = string
   description = "String contains cgroup node config for Autopilot node pools"
 
-  default = null
+  default = ""
 
   validation {
-    condition = var.node_pools_cgroup_mode == null || contains(
+    condition = var.node_pools_cgroup_mode == "" || contains(
       [
         "CGROUP_MODE_UNSPECIFIED",
         "CGROUP_MODE_V1",
@@ -340,7 +340,7 @@ variable "node_pools_cgroup_mode" {
       ],
       var.node_pools_cgroup_mode
     )
-    error_message = "The value for node_pools_cgroup_mode must be one of: CGROUP_MODE_UNSPECIFIED, CGROUP_MODE_V1, CGROUP_MODE_V2, or null."
+    error_message = "The value for node_pools_cgroup_mode must be one of: CGROUP_MODE_UNSPECIFIED, CGROUP_MODE_V1, CGROUP_MODE_V2"
   }
 }
 
