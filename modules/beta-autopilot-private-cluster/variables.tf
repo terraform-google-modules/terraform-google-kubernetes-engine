7d77aa71cdeef037429e1fbb3e9954731a38e531
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2025-02-13 17:14:55 +0100

    chore: rename variable to node_pools_cgroup_mode

--- modules/beta-autopilot-private-cluster/variables.tf
+++ modules/beta-autopilot-private-cluster/variables.tf
@@ -114,12 +114,12 @@ variable "insecure_kubelet_readonly_port_enabled" {
   default     = null
 }
 
-variable "linux_cgroup_mode" {
+variable "node_pools_cgroup_mode" {
   type        = string
   description = "Specifies the Linux cgroup mode for autopilot Kubernetes nodes in the cluster. Accepted values are `CGROUP_MODE_UNSPECIFIED`, `CGROUP_MODE_V1`, and `CGROUP_MODE_V2`, which determine the control group hierarchy used for resource management."
   validation {
-    condition     = var.linux_cgroup_mode == null || contains(["", "CGROUP_MODE_UNSPECIFIED", "CGROUP_MODE_V1", "CGROUP_MODE_V2"], var.linux_cgroup_mode != null ? var.linux_cgroup_mode : "")
-    error_message = "The linux_cgroup_mode must be one of CGROUP_MODE_UNSPECIFIED, CGROUP_MODE_V1, or CGROUP_MODE_V2."
+    condition     = var.node_pools_cgroup_mode == null || contains(["", "CGROUP_MODE_UNSPECIFIED", "CGROUP_MODE_V1", "CGROUP_MODE_V2"], var.node_pools_cgroup_mode != null ? var.node_pools_cgroup_mode : "")
+    error_message = "The node_pools_cgroup_mode must be one of CGROUP_MODE_UNSPECIFIED, CGROUP_MODE_V1, or CGROUP_MODE_V2."
   }
   default = null
 }
