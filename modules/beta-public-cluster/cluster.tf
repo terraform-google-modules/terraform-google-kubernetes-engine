8881fadc616434d49b604362037858d7fa42e155
drfaust92 <ilia.lazebnik@gmail.com>
2025-04-11 09:05:06 -0400

    add local_ssd_encryption_mode
    Signed-off-by: drfaust92 <ilia.lazebnik@gmail.com>

--- modules/beta-public-cluster/cluster.tf
+++ modules/beta-public-cluster/cluster.tf
@@ -543,6 +543,8 @@ resource "google_container_cluster" "primary" {
         enable_secure_boot          = lookup(var.node_pools[0], "enable_secure_boot", false)
         enable_integrity_monitoring = lookup(var.node_pools[0], "enable_integrity_monitoring", true)
       }
+
+      local_ssd_encryption_mode = lookup(var.node_pools[0], "local_ssd_encryption_mode", "")
     }
   }
 
@@ -953,6 +955,7 @@ resource "google_container_node_pool" "pools" {
       }
     }
 
+    local_ssd_encryption_mode = lookup(each.value, "local_ssd_encryption_mode", "")
   }
 
   lifecycle {
@@ -1273,6 +1276,7 @@ resource "google_container_node_pool" "windows_pools" {
       }
     }
 
+    local_ssd_encryption_mode = lookup(each.value, "local_ssd_encryption_mode", "")
   }
 
   lifecycle {
