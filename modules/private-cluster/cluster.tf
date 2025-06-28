147439de1cd55ba595f0c0d5c2c3521734f7612a
drfaust92 <ilia.lazebnik@gmail.com>
2025-06-27 22:57:18 -0400

    feat: add support for max_run_duration
    Signed-off-by: drfaust92 <ilia.lazebnik@gmail.com>

--- modules/private-cluster/cluster.tf
+++ modules/private-cluster/cluster.tf
@@ -525,6 +525,7 @@ resource "google_container_cluster" "primary" {
       }
 
       local_ssd_encryption_mode = lookup(var.node_pools[0], "local_ssd_encryption_mode", null)
+      max_run_duration          = lookup(var.node_pools[0], "max_run_duration", null)
     }
   }
 
@@ -974,6 +975,7 @@ resource "google_container_node_pool" "pools" {
     }
 
     local_ssd_encryption_mode = lookup(each.value, "local_ssd_encryption_mode", null)
+    max_run_duration          = lookup(each.value, "max_run_duration", null)
   }
 
   lifecycle {
@@ -1295,6 +1297,7 @@ resource "google_container_node_pool" "windows_pools" {
     }
 
     local_ssd_encryption_mode = lookup(each.value, "local_ssd_encryption_mode", null)
+    max_run_duration          = lookup(each.value, "max_run_duration", null)
   }
 
   lifecycle {
