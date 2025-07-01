d80316b964a8d46a30db6c2cf90b7e95335e3467
Ilia Lazebnik <Ilia.lazebnik@gmail.com>
2025-07-02 01:05:50 +0300

    feat: add support for max_run_duration (#2374)
    Signed-off-by: drfaust92 <ilia.lazebnik@gmail.com>

--- modules/beta-private-cluster-update-variant/cluster.tf
+++ modules/beta-private-cluster-update-variant/cluster.tf
@@ -570,6 +570,7 @@ resource "google_container_cluster" "primary" {
       }
 
       local_ssd_encryption_mode = lookup(var.node_pools[0], "local_ssd_encryption_mode", null)
+      max_run_duration          = lookup(var.node_pools[0], "max_run_duration", null)
     }
   }
 
@@ -712,6 +713,7 @@ locals {
     "reservation_affinity_values",
     "secondary_boot_disk",
     "local_ssd_encryption_mode",
+    "max_run_duration",
   ]
 }
 
@@ -1115,6 +1117,7 @@ resource "google_container_node_pool" "pools" {
     }
 
     local_ssd_encryption_mode = lookup(each.value, "local_ssd_encryption_mode", null)
+    max_run_duration          = lookup(each.value, "max_run_duration", null)
   }
 
   lifecycle {
@@ -1450,6 +1453,7 @@ resource "google_container_node_pool" "windows_pools" {
     }
 
     local_ssd_encryption_mode = lookup(each.value, "local_ssd_encryption_mode", null)
+    max_run_duration          = lookup(each.value, "max_run_duration", null)
   }
 
   lifecycle {
