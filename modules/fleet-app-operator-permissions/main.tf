2d9c5d6fdf20082b4bcd67517f2296d735bf54f1
tonybayvas <tonybayvas@google.com>
2025-07-01 02:26:33 -0700

    Add custom fields to mappings

--- modules/fleet-app-operator-permissions/main.tf
+++ modules/fleet-app-operator-permissions/main.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2024 Google LLC
+ * Copyright 2025 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,12 +30,14 @@ locals {
     "VIEW"  = "roles/gkehub.scopeViewerProjectLevel"
     "EDIT"  = "roles/gkehub.scopeEditorProjectLevel"
     "ADMIN" = "roles/gkehub.scopeEditorProjectLevel" # Same as EDIT
+    "CUSTOM" = "roles/gkehub.scopeEditorProjectLevel" # Same as EDIT
   }
 
   resource_level_scope_role = {
     "VIEW"  = "roles/gkehub.scopeViewer"
     "EDIT"  = "roles/gkehub.scopeEditor"
     "ADMIN" = "roles/gkehub.scopeAdmin"
+    "CUSTOM" = "roles/gkehub.scopeViewer" # Same as VIEW
   }
 }
 
