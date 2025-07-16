ae915e2a7d4202f96429e905d89eb0d440cd09c1
tonybayvas <tonybayvas@google.com>
2025-07-16 13:10:19 -0700

    Update main.tf

--- modules/fleet-app-operator-permissions/main.tf
+++ modules/fleet-app-operator-permissions/main.tf
@@ -60,7 +60,7 @@ resource "google_project_iam_member" "project_level_scope_permissions" {
   member   = each.value
 }
 
-resource "google_gke_hub_scope_iam_binding" "resource_level_scope_permissions" {
+resource "google_gke_hub_scope_iam_member" "resource_level_scope_permissions" {
   project  = var.fleet_project_id
   scope_id = var.scope_id
   role     = (var.custom_role != null ? local.resource_level_scope_role["CUSTOM"] : local.resource_level_scope_role[var.role])
