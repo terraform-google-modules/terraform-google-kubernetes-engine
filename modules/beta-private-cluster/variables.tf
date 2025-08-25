f7fbc6c205bed545525cdc985b56b6b29715bb58
Andrew Peabody <andrewpeabody@google.com>
2025-08-25 20:46:48 +0000

    fix: note org support of Identity Service for GKE

--- modules/beta-private-cluster/variables.tf
+++ modules/beta-private-cluster/variables.tf
@@ -1022,7 +1022,7 @@ variable "sandbox_enabled" {
 
 variable "enable_identity_service" {
   type        = bool
-  description = "(Optional) Enable the Identity Service component, which allows customers to use external identity providers with the K8S API."
+  description = "(Optional) Enable the Identity Service component, which allows customers to use external identity providers with the K8S API. NOTE: Starting on July 1, 2025, new Google Cloud organizations that you create won't support Identity Service for GKE."
   default     = false
 }
 
