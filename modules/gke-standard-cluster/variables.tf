5f7d53cd77dd593508856fd84472b7071bd7b779
Daisy prakash khoja <70110042+Daisyprakash@users.noreply.github.com>
2025-12-04 03:02:05 +0530

    fix!: making location field as required for standard cluster (#2495)
    Co-authored-by: Andrew Peabody <andrewpeabody@google.com>

--- modules/gke-standard-cluster/variables.tf
+++ modules/gke-standard-cluster/variables.tf
@@ -34,7 +34,6 @@ variable "project_id" {
 variable "location" {
   description = "The location (region or zone) in which the cluster master will be created, as well as the default node location. If you specify a zone (such as us-central1-a), the cluster will be a zonal cluster with a single cluster master. If you specify a region (such as us-west1), the cluster will be a regional cluster with multiple masters spread across zones in the region, and with default node locations in those zones as well."
   type        = string
-  default     = null
 }
 
 variable "network" {
