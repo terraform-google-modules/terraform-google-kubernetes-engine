1ad1719b34c9f26556b1cf808c8801ef38043276
Andrew Peabody <andrewpeabody@google.com>
2025-12-19 23:10:23 +0000

    fix: add nonsensitive for TPG v7.14.0

--- outputs.tf
+++ outputs.tf
@@ -58,7 +58,7 @@ output "zones" {
 
 output "endpoint" {
   description = "Cluster endpoint"
-  value       = local.cluster_endpoint
+  value       = nonsensitive(local.cluster_endpoint)
   depends_on = [
     /* Nominally, the endpoint is populated as soon as it is known to Terraform.
     * However, the cluster may not be in a usable state yet.  Therefore any
