75b37990a718fe58e4e21153f10d7ad25cbff3bd
litzius <mneubert97@gmail.com>
2026-05-28 22:21:33 +0200

    fix: remove extra blank line after secret_sync_config block in cluster template

--- modules/beta-autopilot-public-cluster/cluster.tf
+++ modules/beta-autopilot-public-cluster/cluster.tf
@@ -183,7 +183,6 @@ resource "google_container_cluster" "primary" {
       enabled = var.enable_managed_machine_learning_diagnostics
     }
   }
-
   dynamic "pod_autoscaling" {
     for_each = length(var.hpa_profile) > 0 ? [1] : []
     content {
