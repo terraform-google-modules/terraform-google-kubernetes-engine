cf1b21d64e45822bc523dbb9fdbf62dafa76a625
Andrew Peabody <andrewpeabody@google.com>
2026-06-05 21:05:14 +0000

    chore: update for new nodeImageConfig block

--- test/integration/simple_regional_cluster_autoscaling/simple_regional_cluster_autoscaling_test.go
+++ test/integration/simple_regional_cluster_autoscaling/simple_regional_cluster_autoscaling_test.go
@@ -37,9 +37,7 @@ func TestSimpleRegionalClusterAutoscaling(t *testing.T) {
 
 		op := gcloud.Runf(t, "container clusters describe %s --zone %s --project %s", clusterName, location, projectId)
 		g := golden.NewOrUpdate(t, op.String(),
-			golden.WithSanitizer(golden.StringSanitizer(serviceAccount, "SERVICE_ACCOUNT")),
-			golden.WithSanitizer(golden.StringSanitizer(projectId, "PROJECT_ID")),
-			golden.WithSanitizer(golden.StringSanitizer(clusterName, "CLUSTER_NAME")),
+			golden.WithSanitizer(testutils.GKEClusterSanitizer(serviceAccount, projectId, clusterName, op)),
 		)
 		validateJSONPaths := []string{
 			"location",
